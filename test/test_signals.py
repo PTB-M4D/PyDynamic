97b423b8ae11a6c91bafa0838a0754d49e1584da
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2018-08-09 19:10:10 +0200

    Adapt tests to nosetest and optimize fixtures

--- test/test_signals.py
+++ test/test_signals.py
@@ -1,63 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Test functions for methods to create testsignals
+Perform tests on methods to create testsignals.
 """
 
 import numpy as np
-import unittest
-print(__name__)
+from nose.tools import assert_almost_equal, assert_less
 
-from PyDynamic.misc.testsignals import *
+from PyDynamic.misc.testsignals import shocklikeGaussian, GaussianPulse, rect, squarepulse
 
-class Test_testsignals(unittest.TestCase):
-	N = 2048
-	Ts= 0.01
-	time = np.arange(0, N*Ts, Ts)
-	t0 = N/2*Ts
-
-	def test_shocklikeGaussian(self):
-		m0 = 1 + np.random.rand()*0.2
-		sigma = 50*self.Ts
-		# zero noise
-		x = shocklikeGaussian(self.time, self.t0, m0, sigma, noise = 0.0)
-		self.assertAlmostEqual(x.max(), m0, places = 5)
-		self.assertLess(np.std(x[:self.N//10]), 1e-10)
-		# noisy signal
-		nstd = 1e-2
-		x = shocklikeGaussian(self.time, self.t0, m0, sigma, noise = nstd)
-		self.assertAlmostEqual(np.round(np.std(x[:self.N//10])*100)/100, nstd)
-
-	def test_GaussianPulse(self):
-		m0 = 1 + np.random.rand()*0.2
-		sigma = 50*self.Ts
-		# zero noise
-		x = GaussianPulse(self.time, self.t0, m0, sigma, noise = 0.0)
-		self.assertAlmostEqual(x.max(), m0)
-		self.assertAlmostEqual(self.time[x.argmax()], self.t0)
-		self.assertLess(np.std(x[:self.N//10]), 1e-10)
-		# noisy signal
-		nstd = 1e-2
-		x = GaussianPulse(self.time, self.t0, m0, sigma, noise = nstd)
-		self.assertAlmostEqual(np.round(np.std(x[:self.N//10])*100)/100, nstd)
-
-	def test_rect(self):
-		width = self.N//4*self.Ts
-		height = 1 + np.random.rand()*0.2
-		x = rect(self.time, self.t0, self.t0+width, height, noise = 0.0)
-		self.assertAlmostEqual(x.max(), height)
-		self.assertLess(np.max(x[self.time<self.t0]), 1e-10)
-		self.assertLess(np.max(x[self.time>self.t0+width]), 1e-10)
-		# noisy signal
-		nstd = 1e-2
-		x = rect(self.time, self.t0, self.t0+width, height, noise = nstd)
-		self.assertAlmostEqual(np.round(np.std(x[self.time<self.t0]) * 100) / 100, nstd)
-
-	def test_squarepulse(self):
-		height = 1 + np.random.rand()*0.2
-		numpulses = 5
-		x = squarepulse(self.time, height, numpulses, noise = 0.0)
-		self.assertAlmostEqual(x.max(), height)
+N = 2048
+Ts = 0.01
+time = np.arange(0, N * Ts, Ts)
+t0 = N / 2 * Ts
 
 
-if __name__ == '__main__':
-	unittest.main()
+def test_shocklikeGaussian():
+    m0 = 1 + np.random.rand() * 0.2
+    sigma = 50 * Ts
+    # zero noise
+    x = shocklikeGaussian(time, t0, m0, sigma, noise=0.0)
+    assert_almost_equal(x.max(), m0, places=5)
+    assert_less(np.std(x[:N // 10]), 1e-10)
+    # noisy signal
+    nstd = 1e-2
+    x = shocklikeGaussian(time, t0, m0, sigma, noise=nstd)
+    assert_almost_equal(np.round(np.std(x[:N // 10]) * 100) / 100, nstd)
+
+
+def test_GaussianPulse():
+    m0 = 1 + np.random.rand() * 0.2
+    sigma = 50 * Ts
+    # zero noise
+    x = GaussianPulse(time, t0, m0, sigma, noise=0.0)
+    assert_almost_equal(x.max(), m0)
+    assert_almost_equal(time[x.argmax()], t0)
+    assert_less(np.std(x[:N // 10]), 1e-10)
+    # noisy signal
+    nstd = 1e-2
+    x = GaussianPulse(time, t0, m0, sigma, noise=nstd)
+    assert_almost_equal(np.round(np.std(x[:N // 10]) * 100) / 100, nstd)
+
+
+def test_rect():
+    width = N // 4 * Ts
+    height = 1 + np.random.rand() * 0.2
+    x = rect(time, t0, t0 + width, height, noise=0.0)
+    assert_almost_equal(x.max(), height)
+    assert_less(np.max(x[time < t0]), 1e-10)
+    assert_less(np.max(x[time > t0 + width]), 1e-10)
+    # noisy signal
+    nstd = 1e-2
+    x = rect(time, t0, t0 + width, height, noise=nstd)
+    assert_almost_equal(np.round(np.std(x[time < t0]) * 100) / 100, nstd)
+
+
+def test_squarepulse():
+    height = 1 + np.random.rand() * 0.2
+    numpulses = 5
+    x = squarepulse(time, height, numpulses, noise=0.0)
+    assert_almost_equal(x.max(), height)
