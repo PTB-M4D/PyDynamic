f0a98cc6a4f319093eacf2d912ba66ce8ecde114
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-07-08 17:56:51 +0200

    Set version number to 1.2.71

--- PyDynamic/__init__.py
+++ PyDynamic/__init__.py
@@ -9,17 +9,19 @@ import uncertainty
 """
 
 from .deconvolution.fit_filter import LSFIR, LSFIR_unc, LSIIR, LSIIR_unc, \
-	LSFIR_uncMC
+    LSFIR_uncMC
 from .identification import *
 from .misc import *
 from .uncertainty import *
 
-__version__ = "1.2.77"
+__version__ = "1.2.71"
 
-__all__ = ['LSFIR','LSIIR','LSFIR_unc','LSFIR_uncMC','LSIIR_unc','fit_sos','FreqResp2RealImag',
-		   'GUM_DFT', 'GUM_iDFT', 'DFT_deconv', 'DFT_multiply', 'DFT2AmpPhase', 'AmpPhase2DFT', 'AmpPhase2Time',
-		   'Time2AmpPhase','FIRuncFilter', 'IIRuncFilter', 'MC', 'SMC',
-		   'db', 'grpdelay', 'mapinside', 'isstable', 'kaiser_lowpass', 'savitzky_golay',
-		   'impinvar', 'sos_absphase', 'sos_realimag', 'sos_FreqResp', 'sos_phys2filter',
-		   'shocklikeGaussian','GaussianPulse','squarepulse','rect','corr_noise',
-		   'print_vec','print_mat','make_semiposdef']
+__all__ = ['LSFIR', 'LSIIR', 'LSFIR_unc', 'LSFIR_uncMC', 'LSIIR_unc', 'fit_sos',
+           'FreqResp2RealImag', 'GUM_DFT', 'GUM_iDFT', 'DFT_deconv',
+           'DFT_multiply', 'DFT2AmpPhase', 'AmpPhase2DFT', 'AmpPhase2Time',
+           'Time2AmpPhase', 'FIRuncFilter', 'IIRuncFilter', 'MC', 'SMC', 'db',
+           'grpdelay', 'mapinside', 'isstable', 'kaiser_lowpass',
+           'savitzky_golay', 'impinvar', 'sos_absphase', 'sos_realimag',
+           'sos_FreqResp', 'sos_phys2filter', 'shocklikeGaussian',
+           'GaussianPulse', 'squarepulse', 'rect', 'corr_noise', 'print_vec',
+           'print_mat', 'make_semiposdef']
