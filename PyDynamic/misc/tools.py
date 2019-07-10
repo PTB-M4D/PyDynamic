6faae6945b5e443790565862a12b2a1cc7a3518f
Maximilian Gruber <gruber04@pad.ptb.de>
2019-07-10 14:57:37 +0200

    remove unused misc.tools
    - removed definitions of col_hstack, find, stack, zerom (and import-artifacts)
    - replaced zerom() with np.zeros() where still in use
    - resolves #45

--- PyDynamic/misc/tools.py
+++ PyDynamic/misc/tools.py
@@ -16,54 +16,6 @@ import scipy.sparse as sparse
 
 __all__ = ['print_mat', 'print_vec', 'make_semiposdef', 'FreqResp2RealImag']
 
-def col_hstack(vectors):
-    """
-    From tuple of 1D ndarrays make a 2D ndarray where the tuple
-    elements are as column vectors horizontally stacked
-    
-    :param vectors: list of K 1D-ndarrays of dimension N
-    :returns matrix: 2D ndarray of shape (N,K)
-    
-    """
-
-    if isinstance(vectors,list):
-        col_vectors = map(lambda x: x[:,np.newaxis], vectors)
-    else:
-        raise ValueError("Input must be of type list\n")    
-    
-    return np.hstack(col_vectors)
-            
-def find(assertions):
-    """
-    MATLAB-like determination of occurence of assertion in an array using the
-    numpy nonzero function
-    """
-    if not isinstance(assertions,tuple):
-        raise ValueError("Input to 'find' needs to be a tuple.")
-        
-    inds = np.arange(len(assertions[0]))
-    for assertion in assertions:
-        inds = inds[np.nonzero(assertion[inds])[0]]
-    
-    return inds
-
-def zerom(shape):
-    """
-    Generate a numpy.matrix of zeros of given shape
-    """      
-    from numpy import zeros, matrix      
-    return matrix(zeros(shape))
-    
-def stack(elements):
-    def make_matrix(v):
-        if len(v.shape())>1:
-            return v
-        else:
-            return v[:,np.newaxis]
-            
-    return np.hstack(map(lambda x: make_matrix(x), elements))
-    
-
 def print_vec(vector,prec=5,retS=False,vertical=False):
     """ Print vector (!D array) to the command line of return as formatted string
 
