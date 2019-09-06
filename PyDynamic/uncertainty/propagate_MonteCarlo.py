2a7ac58619343a3928b9521bfa16a7a2f4af25a7
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-09-06 17:04:07 +0200

    Reformat code in UMC_generic docstring again

--- PyDynamic/uncertainty/propagate_MonteCarlo.py
+++ PyDynamic/uncertainty/propagate_MonteCarlo.py
@@ -596,8 +596,8 @@ def UMC_generic(draw_samples, evaluate, runs = 100, blocksize = 8, runs_init = 1
 
         build a function, that only accepts one argument by masking
         additional kwargs:
-        ``evaluate = functools.partial(_UMCevaluate, nbb=b.size, x=x, Delta=Delta, phi=phi, theta=theta, sigma=sigma, blow=blow, alow=alow)
-        evaluate = functools.partial(bigFunction, **dict_of_kwargs)``
+        ``evaluate = functools.partial(_UMCevaluate, nbb=b.size, x=x, Delta=Delta, phi=phi, theta=theta, sigma=sigma, blow=blow, alow=alow)``
+        ``evaluate = functools.partial(bigFunction, **dict_of_kwargs)``
 
     If ``return_samples`` is ``False``, the method returns:
 
