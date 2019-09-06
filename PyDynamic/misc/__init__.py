c7f120664e7da2b829eedca9919ace18005c86c5
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-09-06 14:08:55 +0200

    Update __init__s and adapt them to PEP8

--- PyDynamic/misc/__init__.py
+++ PyDynamic/misc/__init__.py
@@ -10,8 +10,9 @@ used in the examples and in some of the other implemented routines.
 __all__ = ['db', 'grpdelay', 'mapinside', 'isstable', 'kaiser_lowpass',
            'savitzky_golay', 'impinvar', 'sos_absphase', 'sos_realimag',
            'sos_FreqResp', 'sos_phys2filter', 'shocklikeGaussian',
-           'GaussianPulse', 'squarepulse', 'rect', 'corr_noise', 'print_vec',
-           'print_mat', 'make_semiposdef', 'FreqResp2RealImag']
+           'GaussianPulse', 'squarepulse', 'rect', 'corr_noise',
+           'sine', 'print_vec', 'print_mat', 'make_semiposdef',
+           'FreqResp2RealImag', 'ARMA']
 
 from .SecondOrderSystem import sos_FreqResp, sos_phys2filter, sos_absphase, \
     sos_realimag
@@ -19,5 +20,6 @@ from .filterstuff import db, grpdelay, mapinside, isstable, kaiser_lowpass, \
     savitzky_golay
 from .impinvar import impinvar
 from .testsignals import shocklikeGaussian, GaussianPulse, squarepulse, rect, \
-    corr_noise
+    corr_noise, sine
+from .noise import ARMA
 from .tools import print_mat, print_vec, make_semiposdef, FreqResp2RealImag
