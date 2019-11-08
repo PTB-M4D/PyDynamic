173404de38dcad86762e9b510dd61a9c7a062fdf
Maximilian Gruber <maximilian.gruber@ptb.de>
2019-11-08 10:26:32 +0100

    Changed variable and comment to match cited paper

--- PyDynamic/misc/noise.py
+++ PyDynamic/misc/noise.py
@@ -108,13 +108,12 @@ def power_law_noise(N = None, w = None, color_value = "white", mean = 0.0, std =
 
     # get index of frequencies
     # note:
-    # * this gives [1., 2., 3., ..., N+1] (in accordance with Zhivomirov2018)
-    # * ==> not ~ f^alpha, but rather ~ (k+1)^alpha (with a zero-based index k)
-    # * this could be a point for further discussion
-    freq = np.fft.rfftfreq(N, d=1/N) + 1
+    # * this gives [1., 2., 3., ..., N+1] (in accordance with [Zhivomirov2018])
+    # * ==> not W_filt ~ f^alpha, but rather W_filt ~ k^alpha
+    k = np.linspace(1,N,N)
 
     # generate the filtered spectrum by multiplication with f^(alpha/2)
-    W_filt = W * np.power(freq, alpha/2)
+    W_filt = W * np.power(k, alpha/2)
 
     # calculate the filtered time-series (inverse fourier of modified spectrum)
     w_filt = np.fft.irfft(W_filt, N)
