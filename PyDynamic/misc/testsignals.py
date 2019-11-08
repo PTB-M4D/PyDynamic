3bcc10cf68573af8de07f3356b8443ed09f0b034
Maximilian Gruber <maximilian.gruber@ptb.de>
2019-11-08 11:20:39 +0100

    Update docstring testsignals.rect

--- PyDynamic/misc/testsignals.py
+++ PyDynamic/misc/testsignals.py
@@ -97,8 +97,9 @@ def rect(time, t0, t1, height=1, noise=0.0):
             time instant of rect rhs
         height : float
             signal maximum
-        noise :float, optional
-            std of simulated signal noise
+        noise :float or numpy.ndarray of shape (N,), optional
+            float: standard deviation of additive white gaussian noise
+            ndarray: user-defined additive noise
 
     Returns
     -------
