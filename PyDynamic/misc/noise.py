deae5072b4bb28772df7b03a44e1006ebdaeca35
Maximilian Gruber <gruber04@pad.ptb.de>
2020-03-19 11:11:48 +0100

    fix(nmisc.noise): return single sided ACF

--- PyDynamic/misc/noise.py
+++ PyDynamic/misc/noise.py
@@ -137,8 +137,7 @@ def power_law_acf(N, color_value = "white", std = 1.0):
     alpha = get_alpha(color_value)
 
     # get index of frequencies (see notes of same line at power_law_noise() )
-    steps = (N)//2 + 1
-    k = np.linspace(0, steps, steps) + 1
+    k = np.linspace(0, N, N) + 1
 
     # generate and transform the power spectral density Sww
     Sww = np.power(k, alpha)
@@ -147,10 +146,10 @@ def power_law_acf(N, color_value = "white", std = 1.0):
     #Sww = Sww / np.sum(Sww) * len(k)   # probably unnecessary because of later normalization of Rww
 
     # inverse Fourier-transform to get Autocorrelation from PSD/Sww
-    Rww = np.fft.irfft(Sww, N)
+    Rww = np.fft.irfft(Sww)
     Rww = std**2 * Rww / Rww[0]           # This normalization ensures the given standard-deviation
 
-    return Rww
+    return Rww[:N]
 
 
 def ARMA(length, phi=0.0, theta=0.0, std=1.0):
