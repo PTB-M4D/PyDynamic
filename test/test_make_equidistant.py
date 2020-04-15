10db26a6eb1e34db27b20d15f61f055b3dc49ba7
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-04-15 17:22:17 +0200

    test(make_equidistant): improve testing of _test_make_equidistant.py_ by introducing hypothesis

--- test/test_make_equidistant.py
+++ test/test_make_equidistant.py
@@ -1,32 +1,98 @@
 # -*- coding: utf-8 -*-
 """ Perform tests on the method *make_equidistant*."""
+from typing import Dict, Tuple, Union
 
+import hypothesis.extra.numpy as hnp
+import hypothesis.strategies as st
 import numpy as np
+from hypothesis import given
+from hypothesis.strategies import composite
 from pytest import raises
 
 from PyDynamic.misc.tools import make_equidistant
 
 n = 50
-t, y, uy, dt, kind = np.linspace(0, 1, n), np.random.uniform(size=n), \
-                     np.random.uniform(size=n), 5e-2, 'previous'
+t, y, uy, dt, kind = (
+    np.linspace(0, 1, n),
+    np.random.uniform(size=n),
+    np.random.uniform(size=n),
+    5e-2,
+    "previous",
+)
 
 
-def test_too_short_call_make_equidistant():
+@composite
+def timestamps_values_uncertainties_kind(
+    draw,
+    min_count: int = 2,
+    max_count: int = None,
+    kind_tuple: Tuple[str] = ("linear", "previous", "next", "nearest"),
+) -> Dict[str, Union[np.ndarray, str]]:
+    """Set custom strategy for _hypothesis_ to draw desired input from
+
+    Parameters
+    ----------
+        draw: callable
+            this is a hypothesis internal callable to actually draw from provided
+            strategies
+        min_count: int
+            the minimum number of elements expected inside the arrays of timestamps,
+            measurement values and associated uncertainties
+        max_count: int
+            the maximum number of elements expected inside the arrays of timestamps,
+            measurement values and associated uncertainties
+        kind_tuple: tuple(str), optional
+            the tuple of strings out of "linear", "previous", "next", "nearest",
+            "spline", "lagrange", "least-squares" from which the strategy for the
+            kind randomly chooses. Defaults to the valid options "linear",
+            "previous", "next", "nearest".
+
+    Returns
+    -------
+        A dict containing the randomly generated expected input parameters t, y, uy,
+        dt, kind for make_equidistant()
+    """
+    # Set all common parameters for timestamps, measurements values and associated
+    # uncertainties including allowed ranges and number of elements.
+    shape_for_timestamps = hnp.array_shapes(
+        max_dims=1, min_side=min_count, max_side=max_count
+    )
+    strategy_params = {
+        "dtype": np.float,
+        "shape": shape_for_timestamps,
+        "elements": st.floats(
+            min_value=0, max_value=1e300, allow_nan=False, allow_infinity=False
+        ),
+        "unique": True,
+    }
+    # Draw "original" timestamps.
+    t = draw(hnp.arrays(**strategy_params))
+    # Sort timestamps in ascending order.
+    t.sort()
+    # Reuse "original" timestamps shape for measurements values and associated
+    # uncertainties and draw both.
+    strategy_params["shape"] = np.shape(t)
+    y = draw(hnp.arrays(**strategy_params))
+    uy = draw(hnp.arrays(**strategy_params))
+    dt = draw(
+        st.floats(
+            min_value=(np.max(t) - np.min(t)) * 1e-3,
+            max_value=np.max(t) - np.min(t),
+            exclude_min=True,
+            allow_nan=False,
+            allow_infinity=False,
+        )
+    )
+    kind = draw(st.sampled_from(kind_tuple))
+    return {"t": t, "y": y, "uy": uy, "dt": dt, "kind": kind}
+
+
+@given(timestamps_values_uncertainties_kind())
+def test_too_short_call_make_equidistant(interp_inputs):
     # Check erroneous calls with too few inputs.
     with raises(TypeError):
-        make_equidistant(t)
-        make_equidistant(t, y)
-
-
-def test_wrong_input_lengths_call_make_equidistant():
-    # Check erroneous calls with unequally long inputs.
-    with raises(ValueError):
-        y_n_wrong = n * 2
-        uy_n_wrong = n * 3
-        y_wrong, uy_wrong = np.random.uniform(size=y_n_wrong), \
-                         np.random.uniform(size=uy_n_wrong)
-        make_equidistant(t, y_wrong, uy_wrong)
-
+        make_equidistant(interp_inputs["t"])
+        make_equidistant(interp_inputs["t"], interp_inputs["y"])
 
 def test_wrong_input_order_call_make_equidistant():
     # Check erroneous calls with not ascending timestamps.
