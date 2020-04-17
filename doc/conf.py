f6733b8e2fe5cb73a51258360a43cdffee6fb175
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-04-17 13:13:50 +0200

    chore(sphinx): reintroduce version variable in _conf.py_ for EPUB creation

--- doc/conf.py
+++ doc/conf.py
@@ -70,7 +70,7 @@ author = "S. Eichstädt, M. Gruber, B. Ludwig, T. Bruns, M. Weber, I. Smith"
 # built documents.
 #
 # The short X.Y version.
-# version = "1.3"
+version = "1.3"
 # The full version, including alpha/beta/rc tags.
 # release = "1.3.1"
 
