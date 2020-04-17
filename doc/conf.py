361e41bf27a856256fad5a1f8ff7788045c48c5a
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-04-17 12:16:11 +0200

    chore(sphinx): remove errornous LaTeX docs creation for now

--- doc/conf.py
+++ doc/conf.py
@@ -219,29 +219,29 @@ htmlhelp_basename = "PyDynamicdoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
-    # Latex figure (float) alignment
-    #'figure_align': 'htbp',
-}
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     #'papersize': 'letterpaper',
+#     # The font size ('10pt', '11pt' or '12pt').
+#     #'pointsize': '10pt',
+#     # Additional stuff for the LaTeX preamble.
+#     #'preamble': '',
+#     # Latex figure (float) alignment
+#     #'figure_align': 'htbp',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "PyDynamic.tex",
-        "PyDynamic Documentation",
-        "S Eichstädt, I Smith",
-        "manual",
-    ),
-]
+# latex_documents = [
+#     (
+#         master_doc,
+#         "PyDynamic.tex",
+#         "PyDynamic Documentation",
+#         "S Eichstädt, I Smith",
+#         "manual",
+#     ),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -268,7 +268,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "pydynamic", "PyDynamic Documentation", [author], 1)]
+# man_pages = [(master_doc, "pydynamic", "PyDynamic Documentation", [author], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -279,17 +279,17 @@ man_pages = [(master_doc, "pydynamic", "PyDynamic Documentation", [author], 1)]
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "PyDynamic",
-        "PyDynamic Documentation",
-        author,
-        "PyDynamic",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
+# texinfo_documents = [
+#     (
+#         master_doc,
+#         "PyDynamic",
+#         "PyDynamic Documentation",
+#         author,
+#         "PyDynamic",
+#         "One line description of project.",
+#         "Miscellaneous",
+#     ),
+# ]
 
 # Documents to append as an appendix to all manuals.
 # texinfo_appendices = []
