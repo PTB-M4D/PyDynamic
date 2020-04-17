fd506da25c47103a66cb38549cafeab44ad06500
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-04-17 11:58:30 +0200

    chore(readthedocs): try to resolve `WARNING: html_static_path entry` error on rtd

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ extensions = [
 ]
 
 # This should make SciPy documentation available inside our docs.
-intersphinx_mapping = {"python": ('http://docs.scipy.org/doc/scipy/reference', None)}
+intersphinx_mapping = {"python": ("http://docs.scipy.org/doc/scipy/reference", None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -152,7 +152,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
