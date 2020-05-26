54bb6c31084407acd67a7950e991921851965e26
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-05-26 19:18:05 +0200

    chore(identification): enhance package docstring of _identification_ with deprecation warning

--- PyDynamic/identification/fit_transfer.py
+++ PyDynamic/identification/fit_transfer.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
-"""Collection of methods for the identification of transfer function models
+"""The module :mod:`PyDynamic.identification.fit_transfer` contains several functions
+for the identification of transfer function models.
 
 This module contains the following function:
 
 * :func:`fit_sos`: Fit second-order model to complex-valued frequency response
+
+
+.. deprecated:: 1.2.71
+        The package *identification* will be combined with the package
+        *deconvolution* and renamed to *model_estimation* in the
+        next major release 2.0.0. From version 1.4.1 on you should only use
+        the new package *model_estimation* instead.
 """
+import warnings
 
 import numpy as np
 
 __all__ = ["fit_sos"]
+
+warnings.simplefilter('default')
+warnings.warn(
+    "The package *identification* will be combined with the package "
+    "*deconvolution* and renamed to *model_estimation* in the "
+    "next major release 2.0.0. From version 1.4.1 on you should only use "
+    "the new package *model_estimation* instead.",
+    DeprecationWarning)
+
 
 
 def fit_sos(f, H, UH=None, weighting=None, MCruns=None, scaling=1e-3):
