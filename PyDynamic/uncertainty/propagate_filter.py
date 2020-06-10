fix(propagate_filter): fix correlated uncertainty formula
change wrong implementation of formula (20) in _get_corr_unc

PyDynamic/uncertainty/propagate_filter.py
@@ -336,12 +336,12 @@ def _get_corr_unc(b, a, Ux):
     """
 
     # get impulse response of IIR defined by (b,a)
-    h_theta = dimpulse((b, a, 1), x0 = 0.0, t=np.arange(0, len(Ux), step=1))[1][0]
+    h_theta = np.squeeze(dimpulse((b, a, 1), x0 = 0.0, t=np.arange(0, len(Ux), step=1))[1][0])
     
     # equation (20), note:
     # - for values r<0 or s<0 the contribution to the sum is zero (because h_theta is zero)
     # - Ux is the one-sided autocorrelation and assumed to be zero outside its range
-    corr_unc = np.sum(toeplitz(Ux) + toeplitz(h_theta))
+    corr_unc = np.sum(toeplitz(Ux) * np.outer(h_theta, h_theta))
 
     return corr_unc
 

test/test_propagate_filter.py
@@ -139,12 +139,13 @@ def test_FIR_IIR_identity():
     # input signal + run methods
     sigma_noise = 1e-2                                    # std for input signal
     x = rect(time,100*Ts,250*Ts,1.0,noise=sigma_noise)    # generate input signal
-    Ux = sigma_noise * np.ones_like(x)                    # uncertainty of input signal
+    Ux = sigma_noise * np.ones_like(x)                    # uncertainty/autocorrelation of input signal
     Uab = np.diag(np.zeros((len(a) + len(b) -1)))         # fully certain filter-parameters, otherwise FIR and IIR do not match! (see docstring of IIRuncFilter)
 
-    # run signal through both implementations
-    y_iir, Uy_iir, _ = IIRuncFilter(x, Ux, b, a, Uab=Uab, kind="diag")
-    y_fir, Uy_fir = FIRuncFilter(x, Ux, theta=b, Utheta=Uab, kind="diag")
+    for kind in ["diag", "corr"]:
+        # run signal through both implementations
+        y_iir, Uy_iir, _ = IIRuncFilter(x, Ux, b, a, Uab=Uab, kind=kind)
+        y_fir, Uy_fir = FIRuncFilter(x, Ux, theta=b, Utheta=Uab, kind=kind)
 
-    assert np.allclose(y_fir, y_iir)
-    assert np.allclose(Uy_fir[len(b):], Uy_iir[len(b):])
+        assert np.allclose(y_fir, y_iir)
+        assert np.allclose(Uy_fir[len(b):], Uy_iir[len(b):])