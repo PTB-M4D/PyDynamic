b8816ed0303eb1645eaec203f860fb4d4fb686cf
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-06-10 14:50:34 +0200

    chore(setup.py): update setup.py to take into account new `PyWavelets`

--- setup.py
+++ setup.py
@@ -35,7 +35,15 @@ setup(
         "Source": "https://github.com/PTB-PSt1/PyDynamic/",
         "Tracker": "https://github.com/PTB-PSt1/PyDynamic/issues",
     },
-    install_requires=["ipykernel", "matplotlib", "numpy", "pandas", "scipy", "sympy"],
+    install_requires=[
+        "ipykernel",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "sympy",
+        "PyWavelets",
+    ],
     python_requires=">=3.5",
     classifiers=[
         "Development Status :: 4 - Beta",
