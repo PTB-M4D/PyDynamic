361484db10fd36377d7698bacefef647f47b9a19
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-06-12 10:32:57 +0200

    chore(setup.py): improve on setup.py by introducing the examples extra and versions to the URLs

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Install PyDynamic in Python path.
-"""
+"""Install PyDynamic in Python path and provide all packaging metadata."""
 
 from os import path
 
@@ -19,23 +17,29 @@ def get_readme():
 
 
 setup(
+    metadata_version="2.1",
     name="PyDynamic",
     version=VERSION,
     description="A software package for the analysis of dynamic measurements",
     long_description=get_readme(),
     long_description_content_type="text/markdown",
     url="https://ptb-pst1.github.io/PyDynamic/",
+    download_url="https://github.com/PTB-PSt1/PyDynamic/releases/download/v{0}/"
+    "PyDynamic-{0}.tar.gz".format(VERSION),
     author=u"Sascha Eichstädt, Maximilian Gruber, Björn Ludwig, Thomas Bruns, "
-    u"Ian Smith",
+    "Martin Weber",
     author_email="sascha.eichstaedt@ptb.de",
     keywords="uncertainty dynamic deconvolution metrology",
     packages=find_packages(exclude=["test"]),
     project_urls={
-        "Documentation": "https://pydynamic.readthedocs.io/",
-        "Source": "https://github.com/PTB-PSt1/PyDynamic/",
+        "Documentation": "https://pydynamic.readthedocs.io/en/v{}/".format(VERSION),
+        "Source": "https://github.com/PTB-PSt1/PyDynamic/tree/v{}/".format(VERSION),
         "Tracker": "https://github.com/PTB-PSt1/PyDynamic/issues",
     },
-    install_requires=["ipykernel", "matplotlib", "numpy", "pandas", "scipy", "sympy"],
+    install_requires=["matplotlib", "numpy", "pandas", "scipy"],
+    # This allow to do "pip install PyDynamic[examples]" and get the dependencies to
+    # execute the Jupyter Notebook examples.
+    extras_require={"examples": ["notebook"],},
     python_requires=">=3.5",
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -51,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
         "Typing :: Typed",
     ],
 )
