f96fbd8f27ac19a7a219f5d4cd2714eb7839d094
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-06-14 08:04:57 +0200

    chore(example): use black on realtime_dwt

--- examples/Digital filtering/realtime_dwt.py
+++ examples/Digital filtering/realtime_dwt.py
@@ -19,12 +19,11 @@ from PyDynamic.misc.buffer import TimeSeriesBuffer as Buffer
 from PyDynamic.misc.testsignals import rect
 
 
-
 def main(make_and_save_nice_plot=False):
-    
+
     if make_and_save_nice_plot:
-        rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-        rc('text', usetex=True)
+        rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
+        rc("text", usetex=True)
 
     # basics
     buffer_length = 120
@@ -38,24 +37,30 @@ def main(make_and_save_nice_plot=False):
     ld, hd, lr, hr = wavelet.filter_design("db2")
     dwt_length = 21
 
-    # init multi level wavelet stuff 
+    # init multi level wavelet stuff
     n_levels = 4
-    output_multi_level = [n_levels] + [level for level in list(range(1,n_levels+1))[::-1]]    # highest level twice because we store detail + approx
-    output_multi_buffer_maxlen = [buffer_length // 2**level for level in output_multi_level]
-    output_multi = [Buffer(maxlen=maxlen) for maxlen in output_multi_buffer_maxlen]   # list of buffer (different lengths to approximately cover the same timespan)
+    output_multi_level = [n_levels] + [
+        level for level in list(range(1, n_levels + 1))[::-1]
+    ]  # highest level twice because we store detail + approx
+    output_multi_buffer_maxlen = [
+        buffer_length // 2 ** level for level in output_multi_level
+    ]
+    output_multi = [
+        Buffer(maxlen=maxlen) for maxlen in output_multi_buffer_maxlen
+    ]  # list of buffer (different lengths to approximately cover the same timespan)
     level_states = None
 
     # init plot
     plt.ion()
     fig = plt.figure()
-    ax = fig.subplots(nrows=1+len(output_multi), ncols=1, sharex=True)
+    ax = fig.subplots(nrows=1 + len(output_multi), ncols=1, sharex=True)
 
     # init signal plot
     ax[0].set_ylabel("$X^{(0)}$")
     ax[0].set_ylim([-2, 2])
-    sm, = ax[0].plot(0, 0, "-k")  # signal
-    su, = ax[0].plot(0, 0, ":k")  # upper unc
-    sl, = ax[0].plot(0, 0, ":k")  # lower unc
+    (sm,) = ax[0].plot(0, 0, "-k")  # signal
+    (su,) = ax[0].plot(0, 0, ":k")  # upper unc
+    (sl,) = ax[0].plot(0, 0, ":k")  # lower unc
 
     # init coefficient plots
     c_lines = []
@@ -65,18 +70,20 @@ def main(make_and_save_nice_plot=False):
         else:
             _ax.set_ylabel("$D^{{({0})}}$".format(level))
         _ax.set_ylim(auto=True)
-        ce = _ax.errorbar(0, 0, yerr=0, linewidth=0, elinewidth=2, color="gray", capsize=5)
-        cm = _ax.scatter([0], [0], c='r')  # (detail) coeffs
+        ce = _ax.errorbar(
+            0, 0, yerr=0, linewidth=0, elinewidth=2, color="gray", capsize=5
+        )
+        cm = _ax.scatter([0], [0], c="r")  # (detail) coeffs
         c_lines.append([cm, ce])
-        _ax.set_ylim([-3,3])
+        _ax.set_ylim([-3, 3])
 
     # simulate infinite stream of data
     while True:
         cycle_counter += 1
 
-        #ti = tm.time()
+        # ti = tm.time()
         ti = cycle_counter * cycle_duration
-        ui = 0.15 * (np.sin(2*ti) + 2)
+        ui = 0.15 * (np.sin(2 * ti) + 2)
         xi = np.sin(ti) + np.random.randn() * ui
 
         signal.append(ti, xi, ui)
@@ -85,28 +92,36 @@ def main(make_and_save_nice_plot=False):
         dwt_counter += 1
         if dwt_counter % dwt_length == 0:
             t, v, u = signal.view_last(dwt_length)
-            
+
             # multi level dwt with uncertainty
-            coeffs, Ucoeffs, ol, level_states = wavelet.wave_dec_realtime(v, u, ld, hd, n=n_levels, level_states=level_states)
-            
+            coeffs, Ucoeffs, ol, level_states = wavelet.wave_dec_realtime(
+                v, u, ld, hd, n=n_levels, level_states=level_states
+            )
+
             # assign correct timestamps to the coefficients
-            i0_old = (level_states["counter"] - len(t)) % 2**n_levels
+            i0_old = (level_states["counter"] - len(t)) % 2 ** n_levels
             time_indices = np.arange(i0_old, i0_old + len(t))
 
             # save results to data structure
-            for c, u, buffer, level in zip(coeffs, Ucoeffs, output_multi, output_multi_level):
-                time_indices_level = (time_indices + 1) % 2**level == 0
+            for c, u, buffer, level in zip(
+                coeffs, Ucoeffs, output_multi, output_multi_level
+            ):
+                time_indices_level = (time_indices + 1) % 2 ** level == 0
                 buffer.append_multi(t[time_indices_level], c, u)
 
             dwt_counter = 0
 
             # set dwt length until next cycle
-            dwt_length = random.choice([1, 2, 10, 21]) # could also be constant, e.g. dwt_length = 20
+            dwt_length = random.choice(
+                [1, 2, 10, 21]
+            )  # could also be constant, e.g. dwt_length = 20
             print(dwt_length)
 
         # update plot every 5 iterations
         plot_counter += 1
-        if plot_counter % 5 == 0 and cycle_counter > buffer_length: # skip plotting at startup, when buffer is still not fully filled
+        if (
+            plot_counter % 5 == 0 and cycle_counter > buffer_length
+        ):  # skip plotting at startup, when buffer is still not fully filled
             # update plot
 
             ## get data to plot
@@ -119,8 +134,8 @@ def main(make_and_save_nice_plot=False):
             su.set_xdata(t_signal)
             sl.set_xdata(t_signal)
             sm.set_ydata(v_signal)
-            su.set_ydata(v_signal+u_signal)
-            sl.set_ydata(v_signal-u_signal)
+            su.set_ydata(v_signal + u_signal)
+            sl.set_ydata(v_signal - u_signal)
             ax[0].set_xlim([min(signal.timestamps), max(signal.timestamps)])
 
             # update dwt lines
@@ -132,32 +147,54 @@ def main(make_and_save_nice_plot=False):
                 # change the scatter
                 data = np.c_[t_coeff, v_coeff]
                 c_line[0].set_offsets(data)
-                c_line[0].set_facecolor(["r"]*len(t_coeff))
+                c_line[0].set_facecolor(["r"] * len(t_coeff))
 
                 # change the error bars
                 c_line[1].remove()
-                c_line[1] = _ax.errorbar(t_coeff, v_coeff, yerr=u_coeff, linewidth=0, elinewidth=1.5, color="gray", capsize=3, zorder=0)
-                upper_unc = v_coeff+u_coeff
-                lower_unc = v_coeff-u_coeff
+                c_line[1] = _ax.errorbar(
+                    t_coeff,
+                    v_coeff,
+                    yerr=u_coeff,
+                    linewidth=0,
+                    elinewidth=1.5,
+                    color="gray",
+                    capsize=3,
+                    zorder=0,
+                )
+                upper_unc = v_coeff + u_coeff
+                lower_unc = v_coeff - u_coeff
 
                 if v_coeff.size != 0:
                     lim = [np.min(lower_unc), np.max(upper_unc)]
                     _ax.set_ylim(lim)
-            
+
             # highlight the largest coefficients
-            coeffs_all = [np.array(buffer.values) for i_buffer, buffer in enumerate(output_multi)]
-            coeffs_all_indices = [[(i_buffer, i_coeff) for i_coeff, coeff in enumerate(buffer.values)] for i_buffer, buffer in enumerate(output_multi)]
-            coeffs_joined = list(itertools.chain(*coeffs_all))                      # make 1D list of all coeff-values
-            coeffs_joined_indices = list(itertools.chain(*coeffs_all_indices))      # make 1D list of (i_buffer, i_coeff)
-            coeff_highest = np.argpartition(-np.abs(coeffs_joined), kth=range(10))[:10]  # get 10 biggest (absolut) values
+            coeffs_all = [
+                np.array(buffer.values) for i_buffer, buffer in enumerate(output_multi)
+            ]
+            coeffs_all_indices = [
+                [(i_buffer, i_coeff) for i_coeff, coeff in enumerate(buffer.values)]
+                for i_buffer, buffer in enumerate(output_multi)
+            ]
+            coeffs_joined = list(
+                itertools.chain(*coeffs_all)
+            )  # make 1D list of all coeff-values
+            coeffs_joined_indices = list(
+                itertools.chain(*coeffs_all_indices)
+            )  # make 1D list of (i_buffer, i_coeff)
+            coeff_highest = np.argpartition(-np.abs(coeffs_joined), kth=range(10))[
+                :10
+            ]  # get 10 biggest (absolut) values
             coeff_highest_indicies = [coeffs_joined_indices[i] for i in coeff_highest]
-            
+
             for coeff_index in coeff_highest_indicies:
                 _ax = ax[1::][::-1][coeff_index[0]]
                 c_line = c_lines[::-1][coeff_index[0]]
-                
+
                 tmp_colors = c_line[0].get_facecolor()
-                tmp_colors = ["k" if i == coeff_index[1] else c for i, c in enumerate(tmp_colors)]
+                tmp_colors = [
+                    "k" if i == coeff_index[1] else c for i, c in enumerate(tmp_colors)
+                ]
                 c_line[0].set_facecolor(tmp_colors)
 
             # finally update the plot itself
@@ -175,6 +212,7 @@ def main(make_and_save_nice_plot=False):
                 ex = input("Exit (y/n): ")
                 if ex == "y":
                     exit()
+
 
 if __name__ == "__main__":
     try:
