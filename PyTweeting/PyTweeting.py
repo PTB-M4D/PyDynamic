26d2c5541606f4c9a2e1ed7f290bbf6756017d3e
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-07-30 09:28:50 +0200

    wip(PyTweeting):matched \# directly

--- PyTweeting/PyTweeting.py
+++ PyTweeting/PyTweeting.py
@@ -47,7 +47,7 @@ def format_md_to_unicode(to_format: str) -> str:
     fragment: str
     for fragment in string_list:
         to_add= fragment.replace('*', u'\U000025cf')
-        if '\\#' in fragment:
+        if '#' in fragment:
             to_add=convert_to_bold(fragment)
         resulting_string += to_add + ' '
     return resulting_string
