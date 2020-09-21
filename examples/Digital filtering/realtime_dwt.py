805abdaeeb44ed9823780315a0d0be70dca1e648
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-09-21 11:40:13 +0200

    refactor(examples): use time-series-buffer in realtime_DWT

--- examples/Digital filtering/realtime_dwt.py
+++ examples/Digital filtering/realtime_dwt.py
@@ -13,21 +13,17 @@ import numpy as np
 import scipy.signal as scs
 from matplotlib import rc
 from matplotlib.patches import Circle
+from time_series_buffer import TimeSeriesBuffer as Buffer
 
 import PyDynamic.uncertainty.propagate_DWT as wavelet
-from PyDynamic.misc.buffer import TimeSeriesBuffer as Buffer
 from PyDynamic.misc.testsignals import rect
 
 
-def main(make_and_save_nice_plot=False):
-
-    if make_and_save_nice_plot:
-        rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
-        rc("text", usetex=True)
+def main():
 
     # basics
     buffer_length = 120
-    signal = Buffer(buffer_length)
+    signal = Buffer(maxlen=buffer_length, return_type="arrays")
     cycle_duration = 0.1  # seconds
     plot_counter = 0
     dwt_counter = 0
@@ -46,7 +42,7 @@ def main(make_and_save_nice_plot=False):
         buffer_length // 2 ** level for level in output_multi_level
     ]
     output_multi = [
-        Buffer(maxlen=maxlen) for maxlen in output_multi_buffer_maxlen
+        Buffer(maxlen=maxlen, return_type="arrays") for maxlen in output_multi_buffer_maxlen
     ]  # list of buffer (different lengths to approximately cover the same timespan)
     level_states = None
 
@@ -56,24 +52,24 @@ def main(make_and_save_nice_plot=False):
     ax = fig.subplots(nrows=1 + len(output_multi), ncols=1, sharex=True)
 
     # init signal plot
-    ax[0].set_ylabel("$X^{(0)}$")
+    ax[0].set_ylabel("$x^{(0)}$")
     ax[0].set_ylim([-2, 2])
     (sm,) = ax[0].plot(0, 0, "-k")  # signal
-    (su,) = ax[0].plot(0, 0, ":k")  # upper unc
-    (sl,) = ax[0].plot(0, 0, ":k")  # lower unc
+    (su,) = ax[0].plot(0, 0, ":k", linewidth=0.8)  # upper unc
+    (sl,) = ax[0].plot(0, 0, ":k", linewidth=0.8)  # lower unc
 
     # init coefficient plots
     c_lines = []
     for i, (level, _ax) in enumerate(zip(output_multi_level[::-1], ax[1:])):
         if i == len(ax[1:]) - 1:
-            _ax.set_ylabel("$X^{{({0})}}$".format(level))
+            _ax.set_ylabel("$a^{{({0})}}$".format(level))
         else:
-            _ax.set_ylabel("$D^{{({0})}}$".format(level))
+            _ax.set_ylabel("$d^{{({0})}}$".format(level))
         _ax.set_ylim(auto=True)
         ce = _ax.errorbar(
             0, 0, yerr=0, linewidth=0, elinewidth=2, color="gray", capsize=5
         )
-        cm = _ax.scatter([0], [0], c="r")  # (detail) coeffs
+        cm = _ax.scatter([0], [0], c="r", s=10)  # (detail) coeffs
         c_lines.append([cm, ce])
         _ax.set_ylim([-3, 3])
 
@@ -86,12 +82,12 @@ def main(make_and_save_nice_plot=False):
         ui = 0.15 * (np.sin(2 * ti) + 2)
         xi = np.sin(ti) + np.random.randn() * ui
 
-        signal.append(ti, xi, ui)
+        signal.add(time=ti, val=xi, val_unc=ui)
 
         # run DWT every <dwt_length> iterations
         dwt_counter += 1
         if dwt_counter % dwt_length == 0:
-            t, v, u = signal.view_last(dwt_length)
+            t, _, v, u = signal.show(dwt_length)
 
             # multi level dwt with uncertainty
             coeffs, Ucoeffs, ol, level_states = wavelet.wave_dec_realtime(
@@ -107,7 +103,8 @@ def main(make_and_save_nice_plot=False):
                 coeffs, Ucoeffs, output_multi, output_multi_level
             ):
                 time_indices_level = (time_indices + 1) % 2 ** level == 0
-                buffer.append_multi(t[time_indices_level], c, u)
+                if time_indices_level.sum() > 0: # otherwise error from time-series-buffer
+                    buffer.add(time=t[time_indices_level], val=c, val_unc=u)
 
             dwt_counter = 0
 
@@ -125,9 +122,7 @@ def main(make_and_save_nice_plot=False):
             # update plot
 
             ## get data to plot
-            t_signal = np.array(signal.timestamps)
-            v_signal = np.array(signal.values)
-            u_signal = np.array(signal.uncertainties)
+            t_signal, _, v_signal, u_signal = signal.show(-1)
 
             ## update signal lines
             sm.set_xdata(t_signal)
@@ -136,66 +131,37 @@ def main(make_and_save_nice_plot=False):
             sm.set_ydata(v_signal)
             su.set_ydata(v_signal + u_signal)
             sl.set_ydata(v_signal - u_signal)
-            ax[0].set_xlim([min(signal.timestamps), max(signal.timestamps)])
+            ax[0].set_xlim([min(t_signal), max(t_signal)])
 
             # update dwt lines
             for buffer, _ax, c_line in zip(output_multi[::-1], ax[1:], c_lines):
-                t_coeff = np.array(buffer.timestamps)
-                v_coeff = np.array(buffer.values)
-                u_coeff = np.array(buffer.uncertainties)
+                if len(buffer) > 0: # otherwise error from time-series-buffer
+                    t_coeff, _, v_coeff, u_coeff = buffer.show(-1)
 
-                # change the scatter
-                data = np.c_[t_coeff, v_coeff]
-                c_line[0].set_offsets(data)
-                c_line[0].set_facecolor(["r"] * len(t_coeff))
+                    # change the scatter
+                    data = np.c_[t_coeff, v_coeff]
+                    c_line[0].set_offsets(data)
+                    c_line[0].set_facecolor(["r"] * len(t_coeff))
 
-                # change the error bars
-                c_line[1].remove()
-                c_line[1] = _ax.errorbar(
-                    t_coeff,
-                    v_coeff,
-                    yerr=u_coeff,
-                    linewidth=0,
-                    elinewidth=1.5,
-                    color="gray",
-                    capsize=3,
-                    zorder=0,
-                )
-                upper_unc = v_coeff + u_coeff
-                lower_unc = v_coeff - u_coeff
+                    # change the error bars
+                    c_line[1].remove()
+                    c_line[1] = _ax.errorbar(
+                        t_coeff,
+                        v_coeff,
+                        yerr=u_coeff,
+                        linewidth=0,
+                        elinewidth=1.5,
+                        color="gray",
+                        capsize=3,
+                        zorder=0,
+                    )
+                    upper_unc = v_coeff + u_coeff
+                    lower_unc = v_coeff - u_coeff
 
-                if v_coeff.size != 0:
-                    lim = [np.min(lower_unc), np.max(upper_unc)]
-                    _ax.set_ylim(lim)
+                    if v_coeff.size != 0:
+                        lim = [np.min(lower_unc), np.max(upper_unc)]
+                        _ax.set_ylim(lim)
 
-            # highlight the largest coefficients
-            coeffs_all = [
-                np.array(buffer.values) for i_buffer, buffer in enumerate(output_multi)
-            ]
-            coeffs_all_indices = [
-                [(i_buffer, i_coeff) for i_coeff, coeff in enumerate(buffer.values)]
-                for i_buffer, buffer in enumerate(output_multi)
-            ]
-            coeffs_joined = list(
-                itertools.chain(*coeffs_all)
-            )  # make 1D list of all coeff-values
-            coeffs_joined_indices = list(
-                itertools.chain(*coeffs_all_indices)
-            )  # make 1D list of (i_buffer, i_coeff)
-            coeff_highest = np.argpartition(-np.abs(coeffs_joined), kth=range(10))[
-                :10
-            ]  # get 10 biggest (absolut) values
-            coeff_highest_indicies = [coeffs_joined_indices[i] for i in coeff_highest]
-
-            for coeff_index in coeff_highest_indicies:
-                _ax = ax[1::][::-1][coeff_index[0]]
-                c_line = c_lines[::-1][coeff_index[0]]
-
-                tmp_colors = c_line[0].get_facecolor()
-                tmp_colors = [
-                    "k" if i == coeff_index[1] else c for i, c in enumerate(tmp_colors)
-                ]
-                c_line[0].set_facecolor(tmp_colors)
 
             # finally update the plot itself
             fig.tight_layout()
@@ -206,12 +172,6 @@ def main(make_and_save_nice_plot=False):
             # reset plot counter
             plot_counter = 0
 
-        # stop at specific cycle for easier saving of plots
-        if make_and_save_nice_plot:
-            if cycle_counter % 7 == 0 and cycle_counter > buffer_length:
-                ex = input("Exit (y/n): ")
-                if ex == "y":
-                    exit()
 
 
 if __name__ == "__main__":
