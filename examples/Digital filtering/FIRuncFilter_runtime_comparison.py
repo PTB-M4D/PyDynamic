92985bd9ff12833fd66a9fd5aab97e010de83703
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-10-27 13:20:28 +0100

    wip(examples): prettify output of FIRuncFilter_runtime_comparison

--- examples/Digital filtering/FIRuncFilter_runtime_comparison.py
+++ examples/Digital filtering/FIRuncFilter_runtime_comparison.py
@@ -38,15 +38,24 @@ signal_length = 10000
 sigma_noise = 1e-2  # 1e-5
 signal = sigma_noise * np.random.randn(signal_length)
 
+# print table header
+table_hdr = "{0:<15s} | {1:<15s} | {2:<10s} | {3:<15s}"
+table_fmt = "{0:<15d} | {1:<15d} | {2:<10s} | {3}"
+print(table_hdr.format("signal-length", "filter-length", "case", "uy[:2]"))
+print("-"*68)
+
 for filter_length in filter_lengths:
 
     # prepare filter parameters
     theta = np.random.random(filter_length)
-
+    
     for case in cases:
         runtime, y, uy = evaluate(signal, theta, case)
         results.loc[filter_length, case] = runtime
+        print(table_fmt.format(signal_length, filter_length, case, uy[:2]))
+    print("-"*68)
 
 # The expected result is, that `full` and `zero` should be roughly as fast (same range/magnitude).
 # `none` will be much faster and yields the same results as `zero`.
+print("\n\nThese are the observed execution times (in seconds) for \ndifferent combinations of filter-lengths and `case`:")
 print(results)
