d3f98c7a5c6fb490779c72dcdd9f64b31d8d8a2b
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-11-23 15:12:35 +0100

    test(FIRuncFilter_2): adjust definitions of covariance
    fixes issue with bad condition number of covariance

--- test/test_propagate_filter.py
+++ test/test_propagate_filter.py
@@ -25,10 +25,23 @@ def random_nonnegative_array(length):
 
 
 def random_covariance_matrix(length):
-    # construct a valid (but random) covariance matrix
-    cov = np.cov(np.random.random((length, length)))
-    cov = cov + np.mean(cov)  # to get better condition number
-    return cov
+    """construct a valid (but random) covariance matrix with good condition number"""
+
+    # because np.cov estimates the mean from data, the returned covariance matrix
+    # has one eigenvalue close to numerical zero (rank n-1).
+    # This leads to a singular matrix, which is badly suited to be used as valid
+    # covariance matrix. To circumvent this:
+
+    ## draw random (n+1, n+1) matrix
+    cov = np.cov(np.random.random((length + 1, length + 1)))
+
+    ## calculate SVD
+    u, s, vh = np.linalg.svd(cov, full_matrices=False, hermitian=True)
+
+    ## reassemble a covariance of size (n, n) by discarding the smallest singular value
+    cov_adjusted = (u[:-1, :-1] * s[:-1]) @ vh[:-1, :-1]
+
+    return cov_adjusted
 
 
 def valid_filters():
