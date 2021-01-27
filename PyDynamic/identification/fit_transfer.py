9cd6f47d90fb10ad126270f658cf0f154432b896
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-01-27 20:15:25 +0100

    wip(identification): improve deprecation warnings in PyDynamic.identification

--- PyDynamic/identification/fit_transfer.py
+++ PyDynamic/identification/fit_transfer.py
@@ -11,11 +11,11 @@ __all__ = ["fit_sos"]
 
 
 def fit_sos(*args, **kwargs):
-    """.. deprecated:: 1.4.1 Please use :func:`PyDynamic.model_estimation.fit_som`"""
+    """.. deprecated:: 2.0.0 Please use :func:`PyDynamic.model_estimation.fit_som`"""
     raise DeprecationWarning(
         "The module *identification* is combined with the module *deconvolution* and "
-        "renamed to :mod:`PyDynamic.model_estimation` since the last major release "
+        "renamed to 'PyDynamic.model_estimation' since the last major release "
         "2.0.0. *identification* might be removed any time. Please switch to the "
         "current module immediately. Please use the current function name "
-        ":func:`PyDynamic.model_estimation.fit_som`."
+        "'PyDynamic.model_estimation.fit_som'."
     )
