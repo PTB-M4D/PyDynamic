d3b0dbb5c0589586cc03d8df5065931dc04b0e7f
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-01-27 21:16:10 +0100

    wip(deconvolution): finalize deprecation warnings in PyDynamic.deconvolution

--- PyDynamic/deconvolution/__init__.py
+++ PyDynamic/deconvolution/__init__.py
@@ -3,10 +3,9 @@
 .. deprecated:: 2.0.0
     The module *deconvolution* is combined with the module *identification* and
     renamed to :mod:`PyDynamic.model_estimation` since the last major release 2.0.0.
-    :func:`PyDynamic.model_estimation.fit_filter.invLSIIR_unc` might be removed any
-    time. Please switch to the current module immediately. The previously known
-    functions `LSFIR()`, `LSFIR_unc()`, `LSIIR()`, `LSIIR_unc()`, `LSFIR_uncMC()`
-    were transferred and merged into
+    *deconvolution* might be removed any time. Please switch to the current module
+    immediately. The previously known functions `LSFIR()`, `LSFIR_unc()`, `LSIIR()`,
+    `LSIIR_unc()`, `LSFIR_uncMC()` were transferred and merged into
     :func:`PyDynamic.model_estimation.fit_filter.LSIIR` and
     :func:`PyDynamic.model_estimation.fit_filter.LSFIR`. Set `inv=true` to fit
     against the reciprocal of the frequency response like with the previous versions
