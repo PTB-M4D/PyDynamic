299165ef630ef54bcb877e8a3260038805609e4f
Maximilian Gruber <maximilian.gruber@ptb.de>
2021-03-16 19:18:16 +0100

    feat(propagate_convolution): convolution with full covariance propagation
    - change the convolution implementation to use _fir_filter
    - now has full covariance output

--- PyDynamic/uncertainty/propagate_convolution.py
+++ PyDynamic/uncertainty/propagate_convolution.py
@@ -1,19 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from .propagate_filter import FIRuncFilter
+from numpy.lib.arraysetops import isin
+from .propagate_filter import _fir_filter
 
 __all__ = ["convolve_unc"]
 
 def convolve_unc(x1, U1, x2, U2, mode="full"):
     """
     An implementation of the discrete convolution of two signals with uncertainty propagation.
-
-    The code builds on PyDynamic.uncertainty.FIRuncFilter, as convolution and filter application 
+    The code builds on PyDynamic.uncertainty._fir_filter, as convolution and filter application 
     are mathematically nearly identical. However, boundary effects need to be taken into account.
-    """
 
-    # assume that x1 is the longer signal, otherwise exchange
+    Parameters
+    ----------
+    x1 : np.ndarray
+        first input signal
+    U1 : np.ndarray
+        full 2D-covariance matrix associated with x1
+        if the signal is fully certain, use `Ux = None` (default) to make use of more efficient calculations.
+    x2 : np.ndarray
+        second input signal
+    U2 : np.ndarray
+        full 2D-covariance matrix associated with x2
+        if the signal is fully certain, use `Ux = None` (default) to make use of more efficient calculations.
+    mode : str, optional
+        full:  (default)
+        valid: 
+        same: 
+    
+    Returns
+    -------
+    y : np.ndarray
+        convoluted output signal
+    Uy : np.ndarray
+        full 2D-covariance matrix of y
+
+    References
+    ----------
+    .. seealso:: :mod:`numpy.convolve`
+    .. seealso:: :mod:`PyDynamic.uncertainty.propagate_filter._fir_filter`
+    """
+    
+    # assume that x1 is the longer signal, otherwise exchange (convolution is commutative)
     if len(x1) < len(x2):
         tmp = x1
         x1 = x2
@@ -23,57 +52,43 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
         U1 = U2
         U2 = tmp
 
-    # select FIRuncFilter-kind based on U1
-    if isinstance(U1, float):
-        kind = "float"
-    elif isinstance(U1, np.ndarray):
-        if len(U1.shape) == 1:
-            if U1.size == x1.size:
-                kind = "diag"
-            else:
-                kind = "corr"
-        else:
-            raise NotImplementedError("Method does currently not support full covariance matrix uncertainty specification of longer signal.")
-
-    # adjust U2 to fit required structure
-    if isinstance(U2, float):
-        U2 = np.square(U2) * np.eye(len(x2))
-    elif isinstance(U2, np.ndarray):
-        if len(U2.shape) == 1:
-            U2 = np.diag(U2 ** 2)
-
     # actual computation
     if mode == "valid":
-        # apply FIRuncFilter directly
-        y, Uy = FIRuncFilter(x1, U1, x2, Utheta=U2, kind=kind)
+        # apply _fir_filter directly
+        y, Uy = _fir_filter(x=x1, theta=x2, Ux=U1, Utheta=U2, initial_conditions="zero")
         
         # remove first len(x2)-1 entries from output
         conv = y[len(x2)-1:]
-        Uconv = Uy[len(x2)-1:]
+        Uconv = Uy[len(x2)-1:, len(x2)-1:]
 
     elif mode == "full":
-        # prepend one zero and append len(b)-1 zeros to x1/U1
-        x1_mod = np.pad(x1, (1, len(x2)-1), mode="constant", constant_values=0)
-        U1_mod = np.pad(U1, (1, len(x2)-1), mode="constant", constant_values=0)
+        # append len(b)-1 zeros to x1/U1
+        x1_mod = np.pad(x1, (0, len(x2)-1), mode="constant", constant_values=0)
+        if isinstance(U1, np.ndarray):
+            U1_mod = np.pad(U1, ((0, len(x2)-1), (0, len(x2)-1)), mode="constant", constant_values=0)
+        else:
+            U1_mod = None
 
-        # apply FIRuncFilter
-        y, Uy = FIRuncFilter(x1_mod, U1_mod, x2, Utheta=U2, kind=kind)
+        # apply _fir_filter
+        y, Uy = _fir_filter(x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero")
 
-        # remove first entry from output
-        conv = y[1:]
-        Uconv = Uy[1:]
+        # use output directly
+        conv = y
+        Uconv = Uy
 
     elif mode == "same":
-        # prepend one zero to x1 and append (len(x2)-1)//2
-        x1_mod = np.pad(x1, (1, (len(x2)-1)//2), mode="constant", constant_values=0)
-        U1_mod = np.pad(U1, (1, (len(x2)-1)//2), mode="constant", constant_values=0)
+        # append (len(x2)-1)//2 to x1
+        x1_mod = np.pad(x1, (0, (len(x2)-1)//2), mode="constant", constant_values=0)
+        if isinstance(U1, np.ndarray):
+            U1_mod = np.pad(U1, ((0, (len(x2)-1)//2), (0, (len(x2)-1)//2)), mode="constant", constant_values=0)
+        else:
+            U1_mod = None
+        # apply _fir_filter
+        y, Uy = _fir_filter(x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero")
 
-        # apply FIRuncFilter
-        y, Uy = FIRuncFilter(x1_mod, U1_mod, x2, Utheta=U2, kind=kind)
-
-        # remove first (len(x2)+1)//2 entries from output
-        conv = y[(len(x2)+1)//2:]
-        Uconv = Uy[(len(x2)+1)//2:]
+        # remove first (len(x2)-1)//2 entries from output
+        conv = y[(len(x2)-1)//2:]
+        Uconv = Uy[(len(x2)-1)//2:, (len(x2)-1)//2:]
 
     else:
         raise ValueError("Mode \"{MODE}\" is not supported.".format(MODE=mode))
