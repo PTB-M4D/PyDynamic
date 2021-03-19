2930f6eacb2d8eae3389dce4105d04e560e15178
Maximilian Gruber <maximilian.gruber@ptb.de>
2021-03-19 22:06:33 +0100

    wip(propagate_convolution): black and remove unsed import

--- PyDynamic/uncertainty/propagate_convolution.py
+++ PyDynamic/uncertainty/propagate_convolution.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.linalg import toeplitz
 
 from .propagate_filter import _fir_filter
 
 __all__ = ["convolve_unc"]
 
+
 def convolve_unc(x1, U1, x2, U2, mode="full"):
     """
     An implementation of the discrete convolution of two signals with uncertainty propagation.
-    The code builds on PyDynamic.uncertainty._fir_filter, as convolution and filter application 
+    The code builds on PyDynamic.uncertainty._fir_filter, as convolution and filter application
     are mathematically nearly identical. However, boundary effects need to be taken into account.
 
     Parameters
@@ -48,7 +48,7 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
     .. seealso:: :mod:`scipy.ndimage.convolve1d`
     .. seealso:: :mod:`PyDynamic.uncertainty.propagate_filter._fir_filter`
     """
-    
+
     # if a numpy-mode is chosen, x1 is expected to be the longer signal
     # remeber that pure convolution is commutative
     if len(x1) < len(x2) and mode in ["valid", "full", "same"]:
@@ -64,22 +64,26 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
     if mode == "valid":
         # apply _fir_filter directly
         y, Uy = _fir_filter(x=x1, theta=x2, Ux=U1, Utheta=U2, initial_conditions="zero")
-        
+
         # remove first len(x2)-1 entries from output
-        conv = y[len(x2)-1:]
-        Uconv = Uy[len(x2)-1:, len(x2)-1:]
+        conv = y[len(x2) - 1 :]
+        Uconv = Uy[len(x2) - 1 :, len(x2) - 1 :]
 
     elif mode == "full":
         # append len(b)-1 zeros to x1/U1
-        pad_len = len(x2)-1
+        pad_len = len(x2) - 1
         x1_mod = np.pad(x1, (0, pad_len), mode="constant", constant_values=0)
         if isinstance(U1, np.ndarray):
-            U1_mod = np.pad(U1, ((0, pad_len), (0, pad_len)), mode="constant", constant_values=0)
+            U1_mod = np.pad(
+                U1, ((0, pad_len), (0, pad_len)), mode="constant", constant_values=0
+            )
         else:
             U1_mod = None
 
         # apply _fir_filter
-        y, Uy = _fir_filter(x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero")
+        y, Uy = _fir_filter(
+            x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero"
+        )
 
         # use output directly
         conv = y
@@ -87,15 +91,19 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
 
     elif mode == "same":
         # append (len(x2)-1)//2 to x1
-        pad_len = (len(x2)-1)//2
+        pad_len = (len(x2) - 1) // 2
         x1_mod = np.pad(x1, (0, pad_len), mode="constant", constant_values=0)
         if isinstance(U1, np.ndarray):
-            U1_mod = np.pad(U1, ((0, pad_len), (0, pad_len)), mode="constant", constant_values=0)
+            U1_mod = np.pad(
+                U1, ((0, pad_len), (0, pad_len)), mode="constant", constant_values=0
+            )
         else:
             U1_mod = None
-        
+
         # apply _fir_filter
-        y, Uy = _fir_filter(x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero")
+        y, Uy = _fir_filter(
+            x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero"
+        )
 
         # remove first (len(x2)-1)//2 entries from output
         conv = y[pad_len:]
@@ -105,16 +113,16 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
 
         # scipy.ndimage.convolve1d and numpy.pad use different (but overlapping) terminology
         mode_translation = {
-            "nearest" : "edge",
-            "reflect" : "symmetric",
-            "mirror" : "reflect",
+            "nearest": "edge",
+            "reflect": "symmetric",
+            "mirror": "reflect",
         }
         pad_mode = mode_translation[mode]
 
         # append (len(x2)-1)//2 to x1
         n1 = len(x1)
         n2 = len(x2)
-        pad_len = (n2+1)//2
+        pad_len = (n2 + 1) // 2
         x1_mod = np.pad(x1, (pad_len, pad_len), mode=pad_mode)
         # only append, if U is an array (leave it as None)
         if isinstance(U1, np.ndarray):
@@ -123,13 +131,17 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
             U1_mod = None
 
         # apply _fir_filter
-        y, Uy = _fir_filter(x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero")
+        y, Uy = _fir_filter(
+            x=x1_mod, theta=x2, Ux=U1_mod, Utheta=U2, initial_conditions="zero"
+        )
 
         # remove leading and trailing entries from output
-        conv = y[n2:n2+n1]
-        Uconv = Uy[n2:n2+n1, n2:n2+n1]
+        conv = y[n2 : n2 + n1]
+        Uconv = Uy[n2 : n2 + n1, n2 : n2 + n1]
 
     else:
-        raise ValueError("convolve_unc: Mode \"{MODE}\" is not supported.".format(MODE=mode))
+        raise ValueError(
+            'convolve_unc: Mode "{MODE}" is not supported.'.format(MODE=mode)
+        )
 
     return conv, Uconv
