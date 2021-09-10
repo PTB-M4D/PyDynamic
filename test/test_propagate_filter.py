393ed0b4de702db782d18fb4422b259a3b0dd36b
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-10 17:54:53 +0200

    refactor(conftest): improve test infrastructure and refactor some minor issues in test_propagate_[convolution|filter].py

--- test/test_propagate_filter.py
+++ test/test_propagate_filter.py
@@ -10,6 +10,7 @@ from scipy.signal import lfilter, lfilter_zi
 from PyDynamic.misc.tools import trimOrPad
 from PyDynamic.uncertainty.propagate_filter import _fir_filter, FIRuncFilter
 from PyDynamic.uncertainty.propagate_MonteCarlo import MC
+from .conftest import random_covariance_matrix
 
 
 def random_array(length):
@@ -26,26 +27,6 @@ def random_rightsided_autocorrelation(length):
     array = random_array(length)
     acf = scipy.signal.correlate(array, array, mode="full")
     return acf[len(acf) // 2 :]
-
-
-def random_covariance_matrix(length):
-    """construct a valid (but random) covariance matrix with good condition number"""
-
-    # because np.cov estimates the mean from data, the returned covariance matrix
-    # has one eigenvalue close to numerical zero (rank n-1).
-    # This leads to a singular matrix, which is badly suited to be used as valid
-    # covariance matrix. To circumvent this:
-
-    ## draw random (n+1, n+1) matrix
-    cov = np.cov(np.random.random((length + 1, length + 1)))
-
-    ## calculate SVD
-    u, s, vh = np.linalg.svd(cov, full_matrices=False, hermitian=True)
-
-    ## reassemble a covariance of size (n, n) by discarding the smallest singular value
-    cov_adjusted = (u[:-1, :-1] * s[:-1]) @ vh[:-1, :-1]
-
-    return cov_adjusted
 
 
 def valid_filters():
