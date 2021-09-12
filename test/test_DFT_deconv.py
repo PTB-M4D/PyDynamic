ce5ca15bd9a81eacc3217a0d02a82116a2e8025b
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-12 20:35:03 +0200

    wip(test_dft_deconv): seriously improve test and introduce dramatic performance gains with comparison to old computations

--- test/test_DFT_deconv.py
+++ test/test_DFT_deconv.py
@@ -4,34 +4,78 @@ import sys
 import numpy as np
 import scipy.stats as stats
 from hypothesis import given, HealthCheck, settings
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_almost_equal
 
-from PyDynamic import DFT_deconv
 from PyDynamic.misc.tools import progress_bar
-from .conftest import random_covariance_matrix, two_to_the_k
+from PyDynamic.uncertainty.propagate_DFT import DFT_deconv
+from .conftest import two_to_the_k
 
 
-@given(two_to_the_k(min_k=4, max_k=6))
+@given(two_to_the_k(min_k=2, max_k=4))
 @settings(deadline=None, suppress_health_check=(HealthCheck.function_scoped_fixture,))
-def test_dft_deconv(capsys, n):
-    covariance_scale_minimizer = 1e-4
-    h_complex = np.random.random(n) + 1j * np.random.random(n)
-    h = np.r_[h_complex.real, h_complex.imag]
-    y_complex = np.random.random(n) + 1j * np.random.random(n)
+def test_dft_deconv(
+    capsys, random_complex_vector, random_covariance_matrix_for_complex_vectors, n
+):
+    covariance_scale_minimizer = 1e-3
+    y_complex = random_complex_vector(n)
     y = np.r_[y_complex.real, y_complex.imag]
-    uy = covariance_scale_minimizer * random_covariance_matrix(length=len(y))
-    uh = covariance_scale_minimizer * random_covariance_matrix(length=len(h))
+    uy = covariance_scale_minimizer * random_covariance_matrix_for_complex_vectors(n)
+    h_complex = random_complex_vector(n)
+    h = np.r_[h_complex.real, h_complex.imag]
+    uh = covariance_scale_minimizer * random_covariance_matrix_for_complex_vectors(n)
     x_deconv, u_deconv = DFT_deconv(H=h, Y=y, UH=uh, UY=uy)
-    y_dist = stats.multivariate_normal(mean=y, cov=uy)
-    h_dist = stats.multivariate_normal(mean=h, cov=uh)
-    n = len(uh) - 2
+    n_monte_carlo_runs = 40000
+    y_mc = stats.multivariate_normal.rvs(mean=y, cov=uy, size=n_monte_carlo_runs)
+    h_mc = stats.multivariate_normal.rvs(mean=h, cov=uh, size=n_monte_carlo_runs)
+    y_mc_mean = np.mean(y_mc, axis=0)
+    y_mc_cov = np.cov(y_mc, rowvar=False)
+    h_mc_mean = np.mean(h_mc, axis=0)
+    h_mc_cov = np.cov(h_mc, rowvar=False)
+    mean_tolerance = 7e-3
+    cov_tolerance = 2016
+    assert_allclose(actual=y_mc_mean, desired=y, rtol=mean_tolerance)
+    assert_allclose(actual=y_mc_cov, desired=uy, rtol=cov_tolerance)
+    assert_allclose(actual=h_mc_mean, desired=h, rtol=mean_tolerance)
+    assert_allclose(actual=h_mc_cov, desired=uh, rtol=cov_tolerance)
     y_divided_by_h_mc = []
-    n_monte_carlo_runs = 10000
+    real_complex_divider_index = 2 * n // 2
+    y_mcs_original = np.array(
+        [
+            y_mc[i_monte_carlo_run][:real_complex_divider_index]
+            + 1j * y_mc[i_monte_carlo_run][real_complex_divider_index:]
+            for i_monte_carlo_run in range(n_monte_carlo_runs)
+        ]
+    )
+    h_mcs_original = np.array(
+        [
+            h_mc[i_monte_carlo_run][:real_complex_divider_index]
+            + 1j * h_mc[i_monte_carlo_run][real_complex_divider_index:]
+            for i_monte_carlo_run in range(n_monte_carlo_runs)
+        ]
+    )
+    y_mcs_numpy = (
+        y_mc[..., :real_complex_divider_index]
+        + 1j * y_mc[..., real_complex_divider_index:]
+    )
+    h_mcs_numpy = (
+        h_mc[..., :real_complex_divider_index]
+        + 1j * h_mc[..., real_complex_divider_index:]
+    )
+    y_divided_by_h_mc_complex = y_mcs_numpy / h_mcs_numpy
+    y_divided_by_h_mc_numpy = np.concatenate(
+        (np.real(y_divided_by_h_mc_complex), np.imag(y_divided_by_h_mc_complex)), axis=1
+    )
+    assert_almost_equal(y_mcs_original, y_mcs_numpy)
+    assert_almost_equal(h_mcs_original, h_mcs_numpy)
     for i_monte_carlo_run in range(n_monte_carlo_runs):
-        y_mc = y_dist.rvs()
-        h_mc = h_dist.rvs()
-        y_complex_mc = y_mc[: n // 2 + 1] + 1j * y_mc[n // 2 + 1 :]
-        h_complex_mc = h_mc[: n // 2 + 1] + 1j * h_mc[n // 2 + 1 :]
+        y_complex_mc = (
+            y_mc[i_monte_carlo_run][:real_complex_divider_index]
+            + 1j * y_mc[i_monte_carlo_run][real_complex_divider_index:]
+        )
+        h_complex_mc = (
+            h_mc[i_monte_carlo_run][:real_complex_divider_index]
+            + 1j * h_mc[i_monte_carlo_run][real_complex_divider_index:]
+        )
         y_divided_by_h_mc.append(
             np.r_[
                 np.real(y_complex_mc / h_complex_mc),
@@ -45,7 +89,8 @@ def test_dft_deconv(capsys, n):
                 prefix="Monte Carlo for test_dft_deconv() running:",
                 fout=sys.stdout,
             )
-    y_divided_by_h_mc_mean = np.mean(y_divided_by_h_mc, axis=0)
-    y_divided_by_h_mc_cov = np.cov(y_divided_by_h_mc, rowvar=False)
-    assert_allclose(x_deconv, y_divided_by_h_mc_mean, rtol=5e-3)
-    assert_allclose(u_deconv, y_divided_by_h_mc_cov, rtol=35e1)
+    assert_almost_equal(y_divided_by_h_mc, y_divided_by_h_mc_numpy)
+    y_divided_by_h_mc_mean = np.mean(y_divided_by_h_mc_numpy, axis=0)
+    y_divided_by_h_mc_cov = np.cov(y_divided_by_h_mc_numpy, rowvar=False)
+    assert_allclose(x_deconv, y_divided_by_h_mc_mean, rtol=24e-3)
+    assert_allclose(u_deconv, y_divided_by_h_mc_cov, rtol=640)
