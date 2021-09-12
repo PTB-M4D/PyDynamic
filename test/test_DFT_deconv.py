00769580e57f21836ce20234396d6e2e9d6c5f61
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-12 20:43:15 +0200

    wip(test_dft_deconv): remove all comparisons to old computations

--- test/test_DFT_deconv.py
+++ test/test_DFT_deconv.py
@@ -1,12 +1,10 @@
 """Test PyDynamic.uncertainty.propagate_DFT.DFT_deconv"""
-import sys
 
 import numpy as np
 import scipy.stats as stats
 from hypothesis import given, HealthCheck, settings
-from numpy.testing import assert_allclose, assert_almost_equal
+from numpy.testing import assert_allclose
 
-from PyDynamic.misc.tools import progress_bar
 from PyDynamic.uncertainty.propagate_DFT import DFT_deconv
 from .conftest import two_to_the_k
 
@@ -27,70 +25,26 @@ def test_dft_deconv(
     n_monte_carlo_runs = 40000
     y_mc = stats.multivariate_normal.rvs(mean=y, cov=uy, size=n_monte_carlo_runs)
     h_mc = stats.multivariate_normal.rvs(mean=h, cov=uh, size=n_monte_carlo_runs)
-    y_mc_mean = np.mean(y_mc, axis=0)
-    y_mc_cov = np.cov(y_mc, rowvar=False)
-    h_mc_mean = np.mean(h_mc, axis=0)
-    h_mc_cov = np.cov(h_mc, rowvar=False)
     mean_tolerance = 7e-3
     cov_tolerance = 2016
-    assert_allclose(actual=y_mc_mean, desired=y, rtol=mean_tolerance)
-    assert_allclose(actual=y_mc_cov, desired=uy, rtol=cov_tolerance)
-    assert_allclose(actual=h_mc_mean, desired=h, rtol=mean_tolerance)
-    assert_allclose(actual=h_mc_cov, desired=uh, rtol=cov_tolerance)
-    y_divided_by_h_mc = []
+    assert_allclose(actual=np.mean(y_mc, axis=0), desired=y, rtol=mean_tolerance)
+    assert_allclose(actual=np.cov(y_mc, rowvar=False), desired=uy, rtol=cov_tolerance)
+    assert_allclose(actual=np.mean(h_mc, axis=0), desired=h, rtol=mean_tolerance)
+    assert_allclose(actual=np.cov(h_mc, rowvar=False), desired=uh, rtol=cov_tolerance)
     real_complex_divider_index = 2 * n // 2
-    y_mcs_original = np.array(
-        [
-            y_mc[i_monte_carlo_run][:real_complex_divider_index]
-            + 1j * y_mc[i_monte_carlo_run][real_complex_divider_index:]
-            for i_monte_carlo_run in range(n_monte_carlo_runs)
-        ]
-    )
-    h_mcs_original = np.array(
-        [
-            h_mc[i_monte_carlo_run][:real_complex_divider_index]
-            + 1j * h_mc[i_monte_carlo_run][real_complex_divider_index:]
-            for i_monte_carlo_run in range(n_monte_carlo_runs)
-        ]
-    )
-    y_mcs_numpy = (
+    y_mcs = (
         y_mc[..., :real_complex_divider_index]
         + 1j * y_mc[..., real_complex_divider_index:]
     )
-    h_mcs_numpy = (
+    h_mcs = (
         h_mc[..., :real_complex_divider_index]
         + 1j * h_mc[..., real_complex_divider_index:]
     )
-    y_divided_by_h_mc_complex = y_mcs_numpy / h_mcs_numpy
-    y_divided_by_h_mc_numpy = np.concatenate(
+    y_divided_by_h_mc_complex = y_mcs / h_mcs
+    y_divided_by_h_mc = np.concatenate(
         (np.real(y_divided_by_h_mc_complex), np.imag(y_divided_by_h_mc_complex)), axis=1
     )
-    assert_almost_equal(y_mcs_original, y_mcs_numpy)
-    assert_almost_equal(h_mcs_original, h_mcs_numpy)
-    for i_monte_carlo_run in range(n_monte_carlo_runs):
-        y_complex_mc = (
-            y_mc[i_monte_carlo_run][:real_complex_divider_index]
-            + 1j * y_mc[i_monte_carlo_run][real_complex_divider_index:]
-        )
-        h_complex_mc = (
-            h_mc[i_monte_carlo_run][:real_complex_divider_index]
-            + 1j * h_mc[i_monte_carlo_run][real_complex_divider_index:]
-        )
-        y_divided_by_h_mc.append(
-            np.r_[
-                np.real(y_complex_mc / h_complex_mc),
-                np.imag(y_complex_mc / h_complex_mc),
-            ]
-        )
-        with capsys.disabled():
-            progress_bar(
-                i_monte_carlo_run,
-                n_monte_carlo_runs,
-                prefix="Monte Carlo for test_dft_deconv() running:",
-                fout=sys.stdout,
-            )
-    assert_almost_equal(y_divided_by_h_mc, y_divided_by_h_mc_numpy)
-    y_divided_by_h_mc_mean = np.mean(y_divided_by_h_mc_numpy, axis=0)
-    y_divided_by_h_mc_cov = np.cov(y_divided_by_h_mc_numpy, rowvar=False)
+    y_divided_by_h_mc_mean = np.mean(y_divided_by_h_mc, axis=0)
+    y_divided_by_h_mc_cov = np.cov(y_divided_by_h_mc, rowvar=False)
     assert_allclose(x_deconv, y_divided_by_h_mc_mean, rtol=24e-3)
     assert_allclose(u_deconv, y_divided_by_h_mc_cov, rtol=640)
