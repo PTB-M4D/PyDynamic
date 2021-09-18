cd982dd9202ac2118e46583747337fd096dedbbe
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-18 17:05:25 +0200

    refactor(test_execution_of_dft_notebook_example): apply black and rename file as well as test to better reflect purpose

--- test/test_execution_of_dft_notebook_example.py
+++ test/test_execution_of_dft_notebook_example.py
@@ -2,16 +2,25 @@
 """ Test the DFT example *examples/DFT and iDFT with PyDynamic...ipynb*."""
 
 import numpy as np
-from matplotlib.pyplot import figure, plot, xlim, xlabel, ylabel, \
-    subplot, errorbar, xticks, subplots_adjust
-from numpy import random, fft, sqrt
+from matplotlib.pyplot import (
+    errorbar,
+    figure,
+    plot,
+    subplot,
+    subplots_adjust,
+    xlabel,
+    xlim,
+    xticks,
+    ylabel,
+)
+from numpy import fft, random, sqrt
 from numpy.ma import arange, sin
 from scipy.constants import pi
 
 from PyDynamic import GUM_DFT
 
 
-def test_execute_example():
+def test_run_copy_of_notebook_code():
     np.random.seed(123)
     Fs = 100  # sampling frequency in Hz
     Ts = 1 / Fs  # sampling interval in s
@@ -19,8 +28,11 @@ def test_execute_example():
     time = arange(0, N * Ts, Ts)  # time instants
     noise_std = 0.1  # signal noise standard deviation
     # time domain signal
-    x = sin(2 * pi * Fs / 10 * time) + sin(2 * pi * Fs / 5 * time) + \
-        random.randn(len(time)) * noise_std
+    x = (
+        sin(2 * pi * Fs / 10 * time)
+        + sin(2 * pi * Fs / 5 * time)
+        + random.randn(len(time)) * noise_std
+    )
 
     # Apply DFT with propagation of uncertainties
     X, UX = GUM_DFT(x, noise_std ** 2)
@@ -34,11 +46,11 @@ def test_execute_example():
 
     figure()
     subplot(211)
-    errorbar(f, X[:len(f)], sqrt(UX[:len(f)]))
+    errorbar(f, X[: len(f)], sqrt(UX[: len(f)]))
     ylabel("real part", fontsize=18)
     xticks([])
     subplot(212)
-    errorbar(f, X[len(f):], sqrt(UX[len(f):]))
+    errorbar(f, X[len(f) :], sqrt(UX[len(f) :]))
     ylabel("imaginary part", fontsize=18)
     xlabel("frequency / Hz", fontsize=18)
     subplots_adjust(hspace=0.05)
