5099e0917935092062252b12b481566069c98ae2
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-09-24 11:29:11 +0200

    refactor(uncertainty): optimize imports

--- src/PyDynamic/uncertainty/__init__.py
+++ src/PyDynamic/uncertainty/__init__.py
@@ -13,23 +13,20 @@ series interpolation.
    `GitHub website <https://www.github.com/PTB-M4D/PyDynamic>`_
 """
 
+from .interpolate import interp1d_unc
 from .propagate_DFT import (
-    GUM_DFT,
-    GUM_iDFT,
-    DFT_deconv,
-    DFT_multiply,
-    DFT2AmpPhase,
     AmpPhase2DFT,
     AmpPhase2Time,
+    DFT2AmpPhase,
+    DFT_deconv,
+    DFT_multiply,
+    GUM_DFT,
+    GUM_iDFT,
     Time2AmpPhase,
 )
-
 from .propagate_filter import FIRuncFilter, IIRuncFilter
-
 from .propagate_MonteCarlo import MC, SMC, UMC, UMC_generic
 from ..misc.noise import ARMA
-
-from .interpolation import interp1d_unc
 
 __all__ = [
     "ARMA",
