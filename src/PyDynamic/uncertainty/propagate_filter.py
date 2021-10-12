fix(FIRuncFilter): output shifting returns expected covariance matrix
- fixes a bug of wrong application of numpy.roll onto the covariance matrix of filter output (only shift in one dimension was performed)

src/PyDynamic/uncertainty/propagate_filter.py
@@ -404,7 +404,7 @@ def FIRuncFilter(
         # shift result
         if shift != 0:
             x = np.roll(x, -int(shift))
-            Ux = np.roll(Ux, (-int(shift), -int(shift)))
+            Ux = np.roll(Ux, (-int(shift), -int(shift)), axis=(0, 1))
 
         if return_full_covariance:
             return x, Ux