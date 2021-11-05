25c26e3737615196bb44405cad26c1b95b34fdb1
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-11-05 14:14:43 +0100

    refactor(test_compare_LSFI..._mc_for_fitting_one_over_H): switch to output

--- test/test_LSFIR.py
+++ test/test_LSFIR.py
@@ -508,31 +508,33 @@ def test_compare_LSFIR_with_zero_to_None_uncertainties_with_svd_for_fitting_one_
     suppress_health_check=[
         *settings.default.suppress_health_check,
         HealthCheck.too_slow,
+        HealthCheck.function_scoped_fixture,
     ],
 )
 @pytest.mark.slow
 def test_compare_LSFIR_with_zero_to_None_uncertainties_and_mc_for_fitting_one_over_H(
-    monte_carlo, freqs, sampling_freq, filter_order
+    capsys, monte_carlo, freqs, sampling_freq, filter_order
 ):
-    b_fir_mc = LSFIR(
-        H=monte_carlo["H"],
-        UH=np.zeros_like(monte_carlo["UH"]),
-        N=filter_order,
-        tau=filter_order // 2,
-        f=freqs,
-        Fs=sampling_freq,
-        inv=True,
-        mc_runs=2,
-    )[0]
-    b_fir_none = LSFIR(
-        H=monte_carlo["H"],
-        N=filter_order,
-        tau=filter_order // 2,
-        f=freqs,
-        Fs=sampling_freq,
-        inv=True,
-        UH=None,
-    )[0]
+    with capsys.disabled():
+        b_fir_mc = LSFIR(
+            H=monte_carlo["H"],
+            UH=np.zeros_like(monte_carlo["UH"]),
+            N=filter_order,
+            tau=filter_order // 2,
+            f=freqs,
+            Fs=sampling_freq,
+            inv=True,
+            mc_runs=2,
+        )[0]
+        b_fir_none = LSFIR(
+            H=monte_carlo["H"],
+            N=filter_order,
+            tau=filter_order // 2,
+            f=freqs,
+            Fs=sampling_freq,
+            inv=True,
+            UH=None,
+        )[0]
     assert_allclose(b_fir_mc, b_fir_none)
 
 
