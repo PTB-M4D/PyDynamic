28c7b94ff7d6e208b30bb530c82dede4cdedf60c
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-11-10 14:59:34 +0100

    refactor(test_signals.py): introduce more tests for signal class

--- test/test_signals.py
+++ test/test_signals.py
@@ -1,8 +1,11 @@
 """ Perform tests on methods to create test signals."""
+from typing import Callable, Dict, Union
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
-from hypothesis import given, strategies as hst
+from hypothesis import given, HealthCheck, settings, strategies as hst
+from hypothesis.strategies import composite
 from numpy.testing import assert_almost_equal
 from pytest import approx
 
@@ -15,7 +18,71 @@ from PyDynamic.misc.testsignals import (
     sine,
     squarepulse,
 )
-from .conftest import hypothesis_not_negative_float
+from PyDynamic.signals import Signal
+from .conftest import (
+    hypothesis_bounded_float,
+    hypothesis_covariance_matrix,
+    hypothesis_float_vector,
+    hypothesis_not_negative_float,
+)
+
+
+@composite
+def signal_inputs(
+    draw: Callable,
+    ensure_time_step_to_be_float: bool = False,
+    ensure_uncertainty_array: bool = False,
+    ensure_uncertainty_covariance_matrix: bool = False,
+) -> Dict[str, Union[float, np.ndarray]]:
+    minimum_float = 1e-5
+    maximum_float = 1e-1
+    number_of_samples = draw(hst.integers(min_value=4, max_value=2048))
+    small_positive_float_strategy = hypothesis_bounded_float(
+        min_value=minimum_float, max_value=maximum_float
+    )
+    time_step_and_freq_strategy = hst.one_of(
+        small_positive_float_strategy,
+        hst.just(None),
+    )
+    if ensure_time_step_to_be_float:
+        freq_strategy = small_positive_float_strategy
+    else:
+        freq_strategy = time_step_and_freq_strategy
+    time_step = draw(small_positive_float_strategy)
+    if time_step is None:
+        max_time = number_of_samples * draw(freq_strategy)
+        sampling_frequency = draw(freq_strategy)
+    else:
+        max_time = number_of_samples * time_step
+        sampling_frequency = draw(hst.sampled_from((np.reciprocal(time_step), None)))
+    time = np.arange(0, max_time, time_step)
+    len_time = len(time)
+    values = rect(time, max_time // 4, max_time // 4 * 3)
+    uncertainties_covariance_strategy = (
+        hypothesis_covariance_matrix(number_of_rows=len_time),
+    )
+    uncertainties_array_strategies = uncertainties_covariance_strategy + (
+        hypothesis_float_vector(
+            min_value=minimum_float, max_value=maximum_float, length=len_time
+        ),
+    )
+    if ensure_uncertainty_covariance_matrix:
+        uncertainties_strategies = uncertainties_covariance_strategy
+    elif ensure_uncertainty_array:
+        uncertainties_strategies = uncertainties_array_strategies
+    else:
+        uncertainties_strategies = uncertainties_array_strategies + (
+            small_positive_float_strategy,
+        )
+
+    ux = draw(hst.one_of(uncertainties_strategies))
+    return {
+        "time": time,
+        "values": values,
+        "Ts": time_step,
+        "Fs": sampling_frequency,
+        "uncertainty": ux,
+    }
 
 
 @pytest.fixture(scope="module")
@@ -192,3 +259,94 @@ def test_signal_example(monkeypatch):
     # lambda expression returning None but only for this one test.
     monkeypatch.setattr(plt, "show", lambda: None, raising=True)
     demonstrate_signal()
+
+
+@given(signal_inputs())
+@settings(
+    deadline=None,
+    suppress_health_check=[
+        *settings.default.suppress_health_check,
+        HealthCheck.too_slow,
+    ],
+)
+def test_signal_class_usual_instanciations(inputs):
+    Signal(**inputs)
+
+
+@given(signal_inputs())
+@settings(
+    deadline=None,
+    suppress_health_check=[
+        *settings.default.suppress_health_check,
+        HealthCheck.too_slow,
+    ],
+)
+def test_signal_class_raise_not_implemented(inputs):
+    inputs["values"] = inputs["values"][..., np.newaxis]
+    with pytest.raises(
+        NotImplementedError,
+        match=r"Signal: Multivariate signals are not implemented yet.",
+    ):
+        Signal(**inputs)
+
+
+@given(signal_inputs(ensure_time_step_to_be_float=True))
+@settings(
+    deadline=None,
+    suppress_health_check=[
+        *settings.default.suppress_health_check,
+        HealthCheck.too_slow,
+    ],
+)
+def test_signal_class_raise_value_error_on_non_matching_sampling_freq_and_time_step(
+    inputs,
+):
+    inputs["Fs"] = inputs["Ts"]
+    with pytest.raises(
+        ValueError,
+        match=r"Signal: Sampling interval and sampling frequency are assumed to be "
+        r"approximately multiplicative inverse to each other.*",
+    ):
+        Signal(**inputs)
+
+
+@given(signal_inputs(ensure_uncertainty_array=True))
+@settings(
+    deadline=None,
+    suppress_health_check=[
+        *settings.default.suppress_health_check,
+        HealthCheck.too_slow,
+    ],
+)
+def test_signal_class_raise_value_error_on_non_matching_dimension_of_uncertainties(
+    inputs,
+):
+    inputs["uncertainty"] = inputs["uncertainty"][:-1]
+    with pytest.raises(
+        ValueError,
+        match=r"Signal: if uncertainties are provided as np.ndarray "
+        r"they are expected to match the number of elements of the "
+        r"provided time vector, but uncertainties are of shape.*",
+    ):
+        Signal(**inputs)
+
+
+@given(signal_inputs(ensure_uncertainty_covariance_matrix=True))
+@settings(
+    deadline=None,
+    suppress_health_check=[
+        *settings.default.suppress_health_check,
+        HealthCheck.too_slow,
+    ],
+)
+def test_signal_class_raise_value_error_on_non_square_uncertainties(
+    inputs,
+):
+    inputs["uncertainty"] = inputs["uncertainty"][..., :-1]
+    with pytest.raises(
+        ValueError,
+        match=r"Signal: if uncertainties are provided as 2-dimensional np.ndarray "
+        r"they are expected to resemble a square matrix, but uncertainties are of "
+        r"shape.*",
+    ):
+        Signal(**inputs)
