fd3be7ff9d6dae42f2166f92ec6a15a64d2ae152
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-11-10 10:24:15 +0100

    refactor(DFT_amplitude_phase): make class from example to better be able to call it

--- src/PyDynamic/examples/uncertainty_for_dft/DFT_amplitude_phase.py
+++ src/PyDynamic/examples/uncertainty_for_dft/DFT_amplitude_phase.py
@@ -10,6 +10,7 @@ publication on GUM2DFT.
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from PyDynamic.misc.testsignals import multi_sine
 from PyDynamic.uncertainty.propagate_DFT import (
     AmpPhase2Time,
@@ -18,74 +19,84 @@ from PyDynamic.uncertainty.propagate_DFT import (
     Time2AmpPhase_multi,
 )
 
-# set amplitude values of multi-sine componentens (last parameter is number of components)
-sine_amps = np.random.randint(1, 4, 10)
-# set frequencies of multi-sine components
-sine_freqs = np.linspace(100, 500, len(sine_amps)) * 2 * np.pi
-# define time axis
-dt = 0.0001
-time = np.arange(0.0, 0.2, dt)
-# measurement noise standard deviation (assume white noise)
-sigma_noise = 0.001
-# generate test signal
-testsignal = multi_sine(time, sine_amps, sine_freqs, noise=sigma_noise)
 
-plt.figure(1, figsize=(12, 6))
-plt.plot(time, testsignal)
-plt.xlabel("time in s")
-plt.ylabel("signal amplitude in a.u.")
+class DftAmplitudePhaseExample:
+    def __init__(self):
+        multi_sine_amps = np.random.randint(1, 4, 10)
+        multi_sine_freqs = np.linspace(100, 500, len(multi_sine_amps)) * 2 * np.pi
+        time_step = 0.0001
+        time = np.arange(start=0.0, stop=0.2, step=time_step)
+        measure_noise_std_sigma = 0.001
+        testsignal = multi_sine(
+            time, multi_sine_amps, multi_sine_freqs, noise=measure_noise_std_sigma
+        )
 
-# uncertainty propagation from time domain to frequency domain
-A, P, UAP = Time2AmpPhase(testsignal, sigma_noise ** 2)
-f = GUM_DFTfreq(len(time), dt)
+        plt.figure(1, figsize=(12, 6))
+        plt.plot(time, testsignal)
+        plt.xlabel("time in s")
+        plt.ylabel("signal amplitude in a.u.")
 
-plt.figure(2, figsize=(12, 6))
-plt.errorbar(f, A, np.sqrt(np.diag(UAP)[: len(A)]), fmt=".-")
-plt.xlabel("frequency in Hz")
-plt.ylabel("DFT magnitude values in a.u.")
+        # uncertainty propagation from time domain to frequency domain
+        A, P, UAP = Time2AmpPhase(testsignal, measure_noise_std_sigma ** 2)
+        f = GUM_DFTfreq(len(time), time_step)
 
-# uncertainty propagation from frequency domain to time domain
-x, ux = AmpPhase2Time(A, P, UAP)
+        plt.figure(2, figsize=(12, 6))
+        plt.errorbar(f, A, np.sqrt(np.diag(UAP)[: len(A)]), fmt=".-")
+        plt.xlabel("frequency in Hz")
+        plt.ylabel("DFT magnitude values in a.u.")
 
-plt.figure(3, figsize=(12, 6))
-plt.subplot(211)
-plt.errorbar(
-    time,
-    x,
-    np.sqrt(np.diag(ux)),
-    fmt=".-",
-    label="after DFT and iDFT using amplitude and phase",
-)
-plt.plot(time, testsignal, label="original signal")
-plt.xlabel("time in s")
-plt.ylabel("signal amplitude in a.u.")
-plt.legend()
-plt.subplot(212)
-plt.plot(
-    time,
-    np.sqrt(np.diag(ux)),
-    label="uncertainty after DFT and iDFT using amplitude and phase",
-)
-plt.plot(time, np.ones_like(time) * sigma_noise, label="original uncertainty")
-plt.xlabel("time in s")
-plt.ylabel("uncertainty in a.u.")
-plt.legend()
+        # uncertainty propagation from frequency domain to time domain
+        x, ux = AmpPhase2Time(A, P, UAP)
 
-# # apply the same method to several signals with one call
-M = 10
-testsignals = np.zeros((M, len(time)))
-for m in range(M):
-    testsignals[m, :] = multi_sine(time, sine_amps, sine_freqs, noise=sigma_noise)
+        plt.figure(3, figsize=(12, 6))
+        plt.subplot(211)
+        plt.errorbar(
+            time,
+            x,
+            np.sqrt(np.diag(ux)),
+            fmt=".-",
+            label="after DFT and iDFT using amplitude and phase",
+        )
+        plt.plot(time, testsignal, label="original signal")
+        plt.xlabel("time in s")
+        plt.ylabel("signal amplitude in a.u.")
+        plt.legend()
+        plt.subplot(212)
+        plt.plot(
+            time,
+            np.sqrt(np.diag(ux)),
+            label="uncertainty after DFT and iDFT using amplitude and phase",
+        )
+        plt.plot(
+            time,
+            np.ones_like(time) * measure_noise_std_sigma,
+            label="original uncertainty",
+        )
+        plt.xlabel("time in s")
+        plt.ylabel("uncertainty in a.u.")
+        plt.legend()
 
-# select those frequencies of the 10 % largest magnitude values
-indices = np.argsort(A)[: len(A) // 10]
-# propagate from time to frequency domain and only select the specific frequencies
-A_mult, P_mult, UAP_mult = Time2AmpPhase_multi(
-    testsignals, np.ones(M) * sigma_noise ** 2, selector=indices
-)
+        # # apply the same method to several signals with one call
+        M = 10
+        testsignals = np.zeros((M, len(time)))
+        for m in range(M):
+            testsignals[m, :] = multi_sine(
+                time, multi_sine_amps, multi_sine_freqs, noise=measure_noise_std_sigma
+            )
 
-plt.figure(4, figsize=(12, 6))
-plt.subplot(211)
-plt.plot(f[indices], A_mult.T, linestyle=":")
-plt.subplot(212)
-plt.plot(f[indices], P_mult.T, linestyle=":")
+        # select those frequencies of the 10 % largest magnitude values
+        indices = np.argsort(A)[: len(A) // 10]
+        # propagate from time to frequency domain and select specified frequencies
+        A_multi, P_multi, UAP_multi = Time2AmpPhase_multi(
+            testsignals, np.ones(M) * measure_noise_std_sigma ** 2, selector=indices
+        )
+
+        plt.figure(4, figsize=(12, 6))
+        plt.subplot(211)
+        plt.plot(f[indices], A_multi.T, linestyle=":")
+        plt.subplot(212)
+        plt.plot(f[indices], P_multi.T, linestyle=":")
+
+
+if __name__ == "__main__":
+    DftAmplitudePhaseExample()
