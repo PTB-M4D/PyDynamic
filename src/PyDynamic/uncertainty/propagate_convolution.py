ec4867925f8896c1b7d70beb9c4d0a5f32ad1fc8
mgrub <50821607+mgrub@users.noreply.github.com>
2022-03-30 15:15:24 +0200

    wip(convolve_unc): adjust docstring
    suggested by @anupam-prasad

--- src/PyDynamic/uncertainty/propagate_convolution.py
+++ src/PyDynamic/uncertainty/propagate_convolution.py
@@ -33,7 +33,7 @@ def convolve_unc(x1, U1, x2, U2, mode="full"):
     ----------
     x1 : np.ndarray, (N,)
         first input signal
-    U1 : np.ndarray, (N, N)
+    U1 : np.ndarray, (N,) or (N, N)
         - 1D-array: standard uncertainties associated with x1 (corresponding to uncorrelated entries of x1)
         - 2D-array: full 2D-covariance matrix associated with x1
         - None: corresponds to a fully certain signal x1, results in more efficient calculation (compared to using np.zeros(...))
