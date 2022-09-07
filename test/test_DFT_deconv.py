6a8ea044bac0034776d5aa58c3f0235f476f9a1a
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-09-07 11:53:28 +0200

    refactor(test_DFT_deconv): increase threshold for uncertainties

--- test/test_DFT_deconv.py
+++ test/test_DFT_deconv.py
@@ -81,7 +81,7 @@ def test_dft_deconv(
     assert_allclose(
         x_deconv + x_deconv_shift_away_from_zero,
         monte_carlo_mean + x_deconv_shift_away_from_zero,
-        rtol=4.9e-2,
+        rtol=5e-2,
     )
     assert_allclose(
         u_deconv + u_deconv_shift_away_from_zero,
