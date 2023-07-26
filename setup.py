05651271a28dc8a9c701fcbb2eb47d4f755536bc
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-07-26 16:36:02 +0200

    refactor(setup.py): clean up code according to pylint and mypy

--- setup.py
+++ setup.py
@@ -9,49 +9,63 @@ from setuptools import Command, find_packages, setup
 def get_readme():
     """Get README.md's content"""
     this_directory = path.abspath(path.dirname(__file__))
-    with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-        return f.read()
+    with open(path.join(this_directory, "README.md"), encoding="utf-8") as file:
+        return file.read()
 
 
-def read(rel_path):
+def get_version(rel_path: str) -> str:
+    """Extract __version__ variable's value from a file's content"""
+    for line in read_file_content(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+def read_file_content(rel_path: str) -> str:
+    """Extract a file's content and provide a variable to access it"""
     here = path.abspath(path.dirname(__file__))
-    with codecs.open(path.join(here, rel_path), "r") as fp:
-        return fp.read()
+    with codecs.open(path.join(here, rel_path), "r") as file:
+        return file.read()
 
 
 class Tweet(Command):
+    """Handle the tweeting on executing setup.py tweet"""
+
     _filename: str
 
     _consumer_key: str
     _consumer_secret: str
     _access_token: str
     _access_token_secret: str
-    _twitter_api_auth_handle: "Client"
+    _twitter_api_auth_handle = None
 
     description: str = "Send new tweets to the Twitter API to announce releases"
 
-    user_options: str = [("filename=", "f", "filename containing the tweet")]
+    user_options: list[tuple[str, str, str]] = [
+        ("filename=", "f", "filename containing the tweet")
+    ]
 
     def initialize_options(self):
+        """Set filename to default value"""
         self._filename = "tweet.txt"
 
     def finalize_options(self):
+        """React to invalid circumstance that no filename is set to extract the tweet's content"""
         if self._filename is None:
             raise RuntimeError("Parameter --filename is missing")
 
     def run(self):
+        """Actually organize and conduct the tweeting"""
         from tweepy import Client
 
-        def _set_twitter_api_secrets_from_environment():
+        def set_twitter_api_secrets_from_environment():
             self._consumer_key = os.getenv("CONSUMER_KEY")
             self._consumer_secret = os.getenv("CONSUMER_SECRET")
             self._access_token = os.getenv("ACCESS_TOKEN")
             self._access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-        def _tweet():
-            self._twitter_api_auth_handle.create_tweet(text=read_tweet_from_file())
-
-        def _set_twitter_api_auth_handle():
+        def set_twitter_api_auth_handle():
             self._twitter_api_auth_handle = _raise_error_or_retrieve_handle()
 
         def _raise_error_or_retrieve_handle() -> Client:
@@ -64,7 +78,7 @@ class Tweet(Command):
                         "'CONSUMER_SECRET', 'ACCESS_TOKEN' and 'ACCESS_TOKEN_SECRET' "
                         "have to be set."
                     )
-                raise TypeError(str(type_error_message))
+                raise TypeError from type_error_message
 
         def _initialize_twitter_api_auth_handle() -> Client:
             return Client(
@@ -74,23 +88,17 @@ class Tweet(Command):
                 access_token_secret=self._access_token_secret,
             )
 
+        def tweet():
+            self._twitter_api_auth_handle.create_tweet(text=read_tweet_from_file())
+
         def read_tweet_from_file() -> str:
-            with open(self._filename, "r") as f:
-                content: str = f.read()
+            with open(self._filename, "r", encoding="utf-8") as file:
+                content: str = file.read()
             return content
 
-        _set_twitter_api_secrets_from_environment()
-        _set_twitter_api_auth_handle()
-        _tweet()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+        set_twitter_api_secrets_from_environment()
+        set_twitter_api_auth_handle()
+        tweet()
 
 
 current_release_version = get_version("src/PyDynamic/__init__.py")
@@ -102,8 +110,9 @@ setup(
     long_description=get_readme(),
     long_description_content_type="text/markdown",
     url="https://ptb-m4d.github.io/PyDynamic/",
-    download_url="https://github.com/PTB-M4D/PyDynamic/releases/download/v{0}/"
-    "PyDynamic-{0}.tar.gz".format(current_release_version),
+    download_url=f"https://github.com/PTB-M4D/PyDynamic/releases/download/"
+    f"v{current_release_version}/"
+    f"PyDynamic-{current_release_version}.tar.gz",
     author="Sascha Eichstädt, Maximilian Gruber, Björn Ludwig, Thomas Bruns, "
     "Martin Weber",
     author_email="sascha.eichstaedt@ptb.de",
